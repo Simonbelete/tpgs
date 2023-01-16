30bb6d0fda51f12043c43ce207fbed1510e69d6d
Simonbelete <simonbelete@gmail.com>
2023-01-16 19:38:08 +0300

    flask databse init

--- api/ilri_pfm_api/config.py
+++ api/ilri_pfm_api/config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
 
 class Config:
+    load_dotenv()
+
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 class DevConfig(Config):
     pass
