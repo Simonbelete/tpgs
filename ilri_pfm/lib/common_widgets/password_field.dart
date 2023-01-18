36f6219984a1d5a13d72ccd3ab678267e027113c
Simonbelete <simonbelete@gmail.com>
2023-01-19 01:28:36 +0300

    password field widget

---
+++ ilri_pfm/lib/common_widgets/password_field.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:ilri_pfm/common_widgets/form_text_box.dart';
+
+class PasswordField extends StatelessWidget {
+  final TextEditingController? controller;
+
+  const PasswordField({super.key, this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return FormTextBox(
+      controller: controller,
+    );
+  }
+}
