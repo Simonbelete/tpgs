340b5bfe7df2e02791b24b0d98ce2fe6d0eb11e9
Simonbelete <simonbelete@gmail.com>
2023-01-20 23:29:09 +0300

    sample api key example

--- api/ilri_pfm_api/pfm_api/v1/views.py
+++ api/ilri_pfm_api/pfm_api/v1/views.py
@@ -3,10 +3,12 @@ from rest_framework import viewsets
 from django.http import HttpResponse
 from pfm_api.v1.serializers import UserSerializer
 from pfm_api.models import UserProfile
+from pfm_api.permissions import CheckApiKey
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 class UserProfileViewSet(viewsets.ModelViewSet):
+    permission_classes = [CheckApiKey]
     queryset = UserProfile.objects.all()
