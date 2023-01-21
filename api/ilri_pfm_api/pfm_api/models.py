5304abd6e9c18f29304af86e02072b3cf7b339cb
Simonbelete <simonbelete@gmail.com>
2023-01-21 23:39:24 +0300

    using base mode fiedl

--- api/ilri_pfm_api/pfm_api/models.py
+++ api/ilri_pfm_api/pfm_api/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     bio = models.CharField(max_length=200)
+
+class Farm(BaseModel):
+    
