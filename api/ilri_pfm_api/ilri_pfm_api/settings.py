0093f142d77e08064e9eb112006164aadd2aa96e
Simonbelete <simonbelete@gmail.com>
2023-01-22 20:26:08 +0300

    setting env and setting is super user true

--- api/ilri_pfm_api/ilri_pfm_api/settings.py
+++ api/ilri_pfm_api/ilri_pfm_api/settings.py
@@ -14,6 +14,11 @@ import os
 import environ
 from pathlib import Path
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- api/ilri_pfm_api/pfm_api/models.py
+++ api/ilri_pfm_api/pfm_api/models.py
@@ -25,6 +25,7 @@ class UserManager(BaseUserManager):
         Create and save a SuperUser with the given email and password.
         """
         extra_fields.setdefault("is_admin", False)
+        extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_farmer", True)
         extra_fields.setdefault("is_superuser", False)
         extra_fields.setdefault("is_active", True)
