264e8d64e615de54bc1e8f21f26900b716042b06
Simonbelete <simonbelete@gmail.com>
2023-01-22 18:36:01 +0300

    adding custom user field

--- api/ilri_pfm_api/pfm_api/models.py
+++ api/ilri_pfm_api/pfm_api/models.py
@@ -1,5 +1,66 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils import timezone
+
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_admin", False)
+        extra_fields.setdefault("is_farmer", True)
+        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_active", False)
+
+        # if extra_fields.get("is_farmer") is not True:
+        #     raise ValueError(_("Superuser must have is_farmer=True."))
+        # if extra_fields.get("is_superuser") is not True:
+        #     raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+    def create_adminuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_farmer", True)
+        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_active", True)
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
-    pass
+    objects = UserManager()
+
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_farmer = models.BooleanField(default=True)
+    ate_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
