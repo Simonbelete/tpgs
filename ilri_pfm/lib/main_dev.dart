cee8181f471cb458d611f36d1af32cd10c12b8c4
Simonbelete <simonbelete@gmail.com>
2023-01-23 15:53:36 +0300

    fixing enable multdex

--- ilri_pfm/lib/main_dev.dart
+++ ilri_pfm/lib/main_dev.dart
@@ -7,29 +7,29 @@ import 'package:ilri_pfm/blocs/auth/bloc.dart';
 import 'package:ilri_pfm/router.dart';
 import 'package:ilri_pfm/screens/home_screen.dart';
 import 'package:ilri_pfm/screens/onboarding_screen.dart';
-import 'package:device_preview/device_preview.dart';
+// import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(DevicePreview(
-    enabled: false,
-    builder: (context) => const AppDev(), // Wrap your app
-  ));
+  // runApp(DevicePreview(
+  //   enabled: false,
+  //   builder: (context) => const AppDev(), // Wrap your app
+  // ));
+  runApp(const AppDev());
 }
 
 class AppDev extends StatelessWidget {
   const AppDev({super.key});
-
   @override
   Widget build(BuildContext context) {
     final FirebaseAuth _auth = FirebaseAuth.instance;
     return MultiBlocProvider(
       providers: [BlocProvider(create: ((context) => AuthenticationBloc()))],
       child: MaterialApp(
-          useInheritedMediaQuery: true,
-          locale: DevicePreview.locale(context),
-          builder: DevicePreview.appBuilder,
+          // useInheritedMediaQuery: true,
+          // locale: DevicePreview.locale(context),
+          // builder: DevicePreview.appBuilder,
           theme: defaultTheme,
           debugShowCheckedModeBanner: false,
           title: 'ILRI PFM Development',
