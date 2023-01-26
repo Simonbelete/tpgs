e54c8e15c29a31a4948f78e61668bb52276a0cf8
Simonbelete <simonbelete@gmail.com>
2023-01-26 21:31:06 +0300

    sing in with google desing

--- ilri_pfm/lib/common_widgets/sign_in_with_google_button.dart
+++ ilri_pfm/lib/common_widgets/sign_in_with_google_button.dart
@@ -7,7 +7,24 @@ class SignInWithGoogleButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: onPressed, child: const Text('Sign in with google'));
+    return OutlinedButton(
+        style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(7.0)),
+            padding:
+                const EdgeInsets.symmetric(vertical: 15, horizontal: 20.0)),
+        onPressed: onPressed,
+        child: Row(
+          children: [
+            Image.asset(
+              'assets/images/google.png',
+              height: 20,
+            ),
+            const SizedBox(
+              width: 20.0,
+            ),
+            const Text('Sign in with google'),
+          ],
+        ));
   }
 }
