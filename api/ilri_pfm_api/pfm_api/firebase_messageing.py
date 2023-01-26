8040b560fcfe4cf51daeb68fbac0b86df7b620ad
Simonbelete <simonbelete@gmail.com>
2023-01-27 00:39:49 +0300

    message notify from backend

--- api/ilri_pfm_api/pfm_api/firebase_messageing.py
+++ api/ilri_pfm_api/pfm_api/firebase_messageing.py
@@ -4,20 +4,20 @@ import firebase_admin
 default_app = firebase_admin.initialize_app()
 
 class FirebaseMessaging:
-    def send_message(self, registration_token, title, body, data=None) -> Any:
+    def send_message(self, token, title, body, data=None):
         message = messaging.Message(
             notification=messaging.Notification(
                 title=title,
                 body=body,
             ),
             data=data,
-            token=registration_token
+            token=token
         )
         response = messaging.send(message)
         print(response)
         return response
 
-    def send_multicast_message(self, registration_tokens, title, body, data=None) -> Any:
+    def send_multicast_message(self, registration_tokens, title, body, data=None):
         # registration_tokens has to be a list
         assert isinstance(registration_tokens, list)
 
