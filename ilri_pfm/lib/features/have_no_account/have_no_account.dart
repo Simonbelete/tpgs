ca0cfdd206b5919df8cb267c465756d542ffce40
Simonbelete <simonbelete@gmail.com>
2023-01-31 09:59:56 +0300

    login ui desing

---
+++ ilri_pfm/lib/features/have_no_account/have_no_account.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:ilri_pfm/app/color_set.dart';
+import 'package:ilri_pfm/common_widgets/sub_title_text.dart';
+import 'package:ilri_pfm/screens/register_screen.dart';
+
+class HaveNoAccount extends StatelessWidget {
+  const HaveNoAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, RegisterScreen.routeName);
+      },
+      child: Row(
+        children: const [
+          SubTitleText(text: 'Dont\'t have account?'),
+          SizedBox(
+            width: 1,
+          ),
+          SubTitleText(
+              text: 'Create new account.',
+              color: kPrimaryColor,
+              fontWeight: FontWeight.bold)
+        ],
+      ),
+    );
+  }
+}
--- ilri_pfm/lib/features/sign_in_form/sign_in_form.dart
+++ ilri_pfm/lib/features/sign_in_form/sign_in_form.dart
@@ -1,9 +1,13 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:ilri_pfm/app/color_set.dart';
 import 'package:ilri_pfm/common_widgets/button.dart';
 import 'package:ilri_pfm/common_widgets/email_field.dart';
+import 'package:ilri_pfm/common_widgets/header_text.dart';
 import 'package:ilri_pfm/common_widgets/password_field.dart';
+import 'package:ilri_pfm/common_widgets/sub_title_text.dart';
+import 'package:ilri_pfm/features/have_no_account/have_no_account.dart';
 import 'package:ilri_pfm/features/sing_in_with_google_form/sing_in_with_google_form.dart';
 import 'package:ilri_pfm/screens/register_screen.dart';
 
@@ -25,25 +29,47 @@ class _SignInFormState extends State<SignInForm> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Container(
         padding: const EdgeInsets.symmetric(horizontal: 30.0),
         child: Column(
           children: [
-            _buildForm(),
-            TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, RegisterScreen.routeName);
-                },
-                child: const Text('Already have account Sign up ')),
-            SignInWithGoogleForm()
+            _buildForm(size),
+            const SizedBox(
+              height: 15,
+            ),
+            const SubTitleText(text: 'Or Login with'),
+            const SizedBox(
+              height: 15,
+            ),
+            SignInWithGoogleForm(),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(width: size.width, child: const HaveNoAccount())
           ],
         ));
   }
 
-  Widget _buildForm() {
+  Widget _buildForm(Size size) {
     return Form(
       key: widget.formKey,
-      child: Column(children: [
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        const HeaderText(
+          text: 'Welcome',
+          fontWeight: FontWeight.normal,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const SubTitleText(text: 'Enter your Email address to sing in.'),
+        const SizedBox(
+          height: 10,
+        ),
+        const HaveNoAccount(),
+        const SizedBox(
+          height: 30,
+        ),
         _emailField(),
         const SizedBox(
           height: 20,
@@ -52,7 +78,24 @@ class _SignInFormState extends State<SignInForm> {
         const SizedBox(
           height: 20,
         ),
-        Button(onPressed: () => widget.onSignIn(), child: Text('Sign in'))
+        Container(
+          width: size.width,
+          child: Center(
+            child: SizedBox(
+              width: size.width * 0.8,
+              child: Button(
+                backgroundColor: kPrimaryColor,
+                color: Colors.white,
+                child: const Text(
+                  'Sign in',
+                ),
+                onPressed: () {
+                  widget.onSignIn();
+                },
+              ),
+            ),
+          ),
+        ),
       ]),
     );
   }
