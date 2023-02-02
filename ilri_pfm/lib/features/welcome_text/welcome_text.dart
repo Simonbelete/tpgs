5df791178e0fe6b7c874379d9628a4fedc1cb79b
Simonbelete <simonbelete@gmail.com>
2023-02-02 11:47:06 +0300

    welcome text feature

---
+++ ilri_pfm/lib/features/welcome_text/welcome_text.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ilri_pfm/blocs/user/bloc.dart';
+import 'package:ilri_pfm/common_widgets/body_text.dart';
+import 'package:ilri_pfm/common_widgets/title_text.dart';
+
+class WelcomeText extends StatelessWidget {
+  const WelcomeText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String? name = context.read<UserBloc>().state.user.name;
+    return Row(
+      children: [TitleText(text: 'Welcome'), BodyText(text: name ?? '')],
+    );
+  }
+}
