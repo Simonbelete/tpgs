7098f95945ec029e3f6649043fea60a28f5eb2a0
Simonbelete <simonbelete@gmail.com>
2023-02-05 17:44:37 +0300

    adding title and hint text to breed type dropdown search

--- ilri_pfm/lib/features/dropdown_searches/breed_type_dropdown_search.dart
+++ ilri_pfm/lib/features/dropdown_searches/breed_type_dropdown_search.dart
@@ -1,29 +1,62 @@
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ilri_pfm/models/breed_type.dart';
 import 'package:ilri_pfm/repository/breed_type_repository.dart';
 
 class BreedTypeDropdownSearch extends StatelessWidget {
+  final String? title;
+  final String? labelText;
+  final String? hintText;
   final Function(BreedType? data) onChange;
 
-  const BreedTypeDropdownSearch({super.key, required this.onChange});
+  const BreedTypeDropdownSearch(
+      {super.key,
+      required this.onChange,
+      this.title,
+      this.hintText,
+      this.labelText});
 
   @override
   Widget build(BuildContext context) {
-    return DropdownSearch<BreedType>(
-        popupProps: const PopupProps.modalBottomSheet(
-          showSearchBox: true,
-        ),
-        dropdownDecoratorProps: const DropDownDecoratorProps(
-          dropdownSearchDecoration: InputDecoration(
-            labelText: "Breed type",
-            hintText: "Select Breed type",
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 4),
+          child: Text(
+            title ?? '',
+            style:
+                GoogleFonts.roboto(fontWeight: FontWeight.bold, fontSize: 13.0),
           ),
         ),
-        asyncItems: (String filter) async {
-          return await BreedTypeRepository().get(query: {'name': filter}) ?? [];
-        },
-        itemAsString: (BreedType u) => u.name,
-        onChanged: onChange);
+        const SizedBox(
+          height: 5,
+        ),
+        DropdownSearch<BreedType>(
+          popupProps: const PopupProps.modalBottomSheet(
+            showSearchBox: true,
+          ),
+          dropdownDecoratorProps: DropDownDecoratorProps(
+            dropdownSearchDecoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                  borderSide: const BorderSide(
+                      color: Color.fromARGB(255, 224, 225, 228))),
+              labelText: labelText ?? "Chicken",
+              hintText: hintText ?? "Select chicken",
+            ),
+          ),
+          asyncItems: (String filter) async {
+            return await BreedTypeRepository().get(query: {'name': filter}) ??
+                [];
+          },
+          itemAsString: (BreedType u) => u.name,
+          onChanged: onChange,
+        ),
+      ],
+    );
   }
 }
