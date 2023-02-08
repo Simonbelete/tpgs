db27e2c67bd74dcc995ffef9a40b6667b685eccf
Simonbelete <simonbelete@gmail.com>
2023-02-09 01:02:54 +0300

    template setup for web

--- ilri_pfm/ilri_pfm/settings.py
+++ ilri_pfm/ilri_pfm/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'web'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'ilri_pfm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
