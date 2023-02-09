6cb87fbe49f11b68fc456c842a4132643fd4e201
Simonbelete <simonbelete@gmail.com>
2023-02-09 09:45:34 +0300

    messages display

--- ilri_pfm/web/views.py
+++ ilri_pfm/web/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def index(request):
@@ -11,9 +12,19 @@ def signIn(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
+        print('----------------------------------')
+        print(email)
+        print(password)
         try:
             user = authenticate(request, email=email, password=password)
-        except:
+            if user is not None:
+                messages.success(request, ' welcome  !!')
+                return redirect('index')
+            else:
+                messages.info(request, f'No user found')
+        except Exception as e:
+            print(e)
             messages.info(request, f'account done not exit please sign in')
 
-    return render(request, 'sign_in.html')
+    form = AuthenticationForm()
+    return render(request, 'sign_in.html', context={'form': form})
