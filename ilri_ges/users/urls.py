9e1693a4fe102c7acce18cc0b184a1a5f062e82d
Simonbelete <simonbelete@gmail.com>
2023-02-23 10:54:00 +0300

    login classbased view

--- ilri_ges/users/urls.py
+++ ilri_ges/users/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.LoginView.as_view(), name='login'),
+    path('login', views.LoginView.as_view(), name='home'),
 ]
--- ilri_ges/users/views.py
+++ ilri_ges/users/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout, login
 
 from users.forms import LoginForm
+
+
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'login/index.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                request, email=form.cleaned_data['email'], password=form.cleaned_data['password'],)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, f'No user found')
+        return render(request, 'login/index.html', {'form': form})
 
 
 def index(request):
