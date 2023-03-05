4f5715746d102adb7518985f26b09091f9c297a2
Simonbelete <simonbelete@gmail.com>
2023-03-05 21:49:39 +0300

    removig slash trail from feed types url

--- ilri_ges/feeds/urls.py
+++ ilri_ges/feeds/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.FeedsView.as_view(), name='feeds'),
     path('<int:id>/', views.FeedsEditView.as_view(), name='feeds_edit'),
     path('create', views.FeedsCreateView.as_view(), name='feeds_create'),
-    path('/types', views.FeedTypesView.as_view(), name='feeds_types'),
-    path('/types/<int:id>/', views.FeedTypesEditView.as_view(),
+    path('types', views.FeedTypesView.as_view(), name='feeds_types'),
+    path('types/<int:id>/', views.FeedTypesEditView.as_view(),
          name='feeds_types_edit'),
-    path('/types/create', views.FeedTypesCreateView.as_view(),
+    path('types/create', views.FeedTypesCreateView.as_view(),
          name='feeds_types_create'),
 ]
