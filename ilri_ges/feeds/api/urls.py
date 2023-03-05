b7d5ac6c150de35b8dbc99ef80fb22188ed2e63e
Simonbelete <simonbelete@gmail.com>
2023-03-05 21:46:14 +0300

    fixing feeds nested routes

--- ilri_ges/feeds/api/urls.py
+++ ilri_ges/feeds/api/urls.py
@@ -4,14 +4,17 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'feeds', views.FeedViewSet, basename='api_feeds'),
-router.register(r'feeds/(?P<id>.+)/histories',
+router.register(r'', views.FeedViewSet, basename='api_feeds'),
+router.register(r'(?P<id>.+)/histories',
                 views.FeedHistoryViewSet, basename='api_feeds_histories'),
-router.register(r'feeds/types', views.FeedTypeViewSet, basename='api_feeds'),
-router.register(r'feeds/types/(?P<id>.+)/histories',
-                views.FeedTypeHistoryViewSet, basename='api_feeds_histories'),
+
+router_types = routers.DefaultRouter()
+router_types.register(r'', views.FeedTypeViewSet, basename='api_feeds'),
+router_types.register(r'(?P<id>.+)/histories',
+                      views.FeedTypeHistoryViewSet, basename='api_feeds_histories'),
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('feeds/types/', include(router_types.urls)),
+    path('feeds/', include(router.urls)),
 ]
