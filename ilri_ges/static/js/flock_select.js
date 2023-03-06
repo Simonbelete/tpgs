ce01b2eab6873f16087bfc3aeae48a4ace1edd0f
Simonbelete <simonbelete@gmail.com>
2023-03-06 21:54:12 +0300

    flock select js

---
+++ ilri_ges/static/js/flock_select.js
@@ -0,0 +1,30 @@
+requirejs(["jquery", "select2"], function ($, Select2) {
+  "use strict";
+  $("#farm_select").select2({
+    theme: "bootstrap4",
+    ajax: {
+      url: "/api/flocks",
+      data: function (params) {
+        var query = {
+          search: params.term,
+          page: params.page || 1,
+        };
+        return query;
+      },
+      processResults: function (data, params) {
+        params.page = params.page || 1;
+        var transform = $.map(data.results, function (obj) {
+          obj.id = obj.id || obj.id;
+          obj.text = obj.text || obj.name;
+          return obj;
+        });
+        return {
+          results: transform,
+          pagination: {
+            more: params.page * 30 < data.count,
+          },
+        };
+      },
+    },
+  });
+});
