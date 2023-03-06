5613db872c388a5fd8419ace545269c14885c9fb
Simonbelete <simonbelete@gmail.com>
2023-03-06 22:31:52 +0300

    farm crud view

--- ilri_ges/farms/models.py
+++ ilri_ges/farms/models.py
@@ -7,7 +7,7 @@ from locations.models import City
 class Farm(CoreModel):
     name = models.CharField(max_length=250, unique=True)
     city = models.ForeignKey(
-        City, on_delete=models.SET_NULL, null=True, related_name='farms')
+        City, on_delete=models.SET_NULL, null=True, blank=True, related_name='farms')
 
     class Meta:
         ordering = ["name"]
--- ilri_ges/farms/views.py
+++ ilri_ges/farms/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate, logout, login
 
 from .forms import FarmForm
+from .models import Farm
 
 
 class FarmsView(View):
@@ -12,23 +14,51 @@ class FarmsView(View):
         return render(request, 'farms/index.html')
 
 
-class FarmsCreateView(View):
+class FarmsCreateView(LoginRequiredMixin, View):
+    ogin_url = '/users/login'
+    redirect_field_name = 'redirect_to'
+
     def get(self, request):
-        form = FarmForm()
+        form = FarmForm
         return render(request, 'farms/create.html', {'form': form})
 
     def post(self, request):
         form = FarmForm(request.POST)
         if form.is_valid():
-            farm = form.save()
-            if farm is not None:
+            form = form.save(commit=False)
+            form.created_by = request.user
+            form.save()
+            if form is not None:
                 return redirect('farms')
             else:
                 messages.error(
-                    request, f'Error occurred while creating farm, please check you data')
+                    request, 'Error occurred while creating, please check your data')
         return render(request, 'farms/create.html', {'form': form})
 
 
-class FarmsEditView(View):
-    def get(self, request):
-        return render(request, 'farms/edit.html')
+class FarmsEditView(LoginRequiredMixin, View):
+    login_url = '/users/login'
+    redirect_field_name = 'redirect_to'
+
+    def get(self, request, id=0):
+        if id == 0:
+            return redirect('404')
+        try:
+            data = Farm.objects.get(pk=id)
+            form = FarmForm(instance=data)
+            return render(request, 'farms/edit.html', {'form': form, "id": id})
+        except Exception as ex:
+            return redirect('500')
+
+    def post(self, request, id=0):
+        if id == 0:
+            return redirect('404')
+        try:
+            data = Farm.objects.get(pk=id)
+            form = FarmForm(request.POST, instance=data)
+            if form.is_valid():
+                form.save()
+            messages.success(request, 'Successfully Updated !')
+            return render(request, 'farms/edit.html', {'form': form})
+        except Exception as ex:
+            return redirect('500')
