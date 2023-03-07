e9d2e26b9bcb4c19778ff3cbe7e9b4299cb639a7
Simonbelete <simonbelete@gmail.com>
2023-03-07 22:55:25 +0300

    total weight for eggs weekly

--- ilri_ges/eggs/forms.py
+++ ilri_ges/eggs/forms.py
@@ -6,4 +6,4 @@ from .models import Egg
 class EggForm(forms.ModelForm):
     class Meta:
         model = Egg
-        fields = ['week', 'eggs', 'chicken']
+        fields = ['week', 'eggs', 'total_weight', 'chicken']
--- ilri_ges/eggs/models.py
+++ ilri_ges/eggs/models.py
@@ -7,6 +7,8 @@ from core.models import CoreModel
 class Egg(CoreModel):
     week = models.IntegerField(default=0)
     eggs = models.IntegerField()
+    total_weight = models.DecimalField(
+        max_digits=10, decimal_places=3, default=0)
     chicken = models.ForeignKey(
         'chickens.Chicken', on_delete=models.CASCADE, related_name='eggs')
 
