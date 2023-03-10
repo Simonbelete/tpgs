1cfbe2dbc13fae1798914f51afac0263a5cd4121
Simonbelete <simonbelete@gmail.com>
2023-03-10 14:13:49 +0300

    fixing breed type count api

--- ilri_ges/breeds/api/urls.py
+++ ilri_ges/breeds/api/urls.py
@@ -4,9 +4,9 @@ from rest_framework import routers
 from breeds.api import views
 
 router = routers.DefaultRouter()
-router.register(r'breed-types', views.BreedTypeViewSet,
+router.register(r'breed-types/count', views.BreedTypeCountViewSet,
                 basename='api_breed_types')
-router.register(r'breed-types/count', views.BreedTypeViewSet,
+router.register(r'breed-types', views.BreedTypeViewSet,
                 basename='api_breed_types')
 
 urlpatterns = [
