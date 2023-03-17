64c6dcfb7a8d15075221da3eab839c40d5ef0406
Simonbelete <simonbelete@gmail.com>
2023-03-17 12:32:57 +0300

    adding reset url with views

--- ilri_ges/users/urls.py
+++ ilri_ges/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -9,10 +10,10 @@ urlpatterns = [
     path('login', views.LoginView.as_view(), name='login'),
     path('logout', views.LogoutView.as_view(), name='logout'),
     path("password_reset", views.PasswordReset.as_view(), name="password_reset"),
-    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-    #     template_name='main/password/password_reset_done.html'), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-    #     template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
-    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-    #     template_name='main/password/password_reset_complete.html'), name='password_reset_complete'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password/password_reset_complete.html'), name='password_reset_complete'),
 ]
