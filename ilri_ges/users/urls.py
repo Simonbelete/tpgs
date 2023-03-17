1cf9d9b358224a9973433b890140b220ae6491eb
Simonbelete <simonbelete@gmail.com>
2023-03-17 11:00:29 +0300

    password reset templates

--- ilri_ges/users/urls.py
+++ ilri_ges/users/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('create', views.UsersCreateView.as_view(), name="users_create"),
     path('login', views.LoginView.as_view(), name='login'),
     path('logout', views.LogoutView.as_view(), name='logout'),
+    # path("password_reset", views.password_reset_request, name="password_reset"),
+    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+    #     template_name='main/password/password_reset_done.html'), name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+    #     template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+    #     template_name='main/password/password_reset_complete.html'), name='password_reset_complete'),
 ]
