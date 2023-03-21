0b55ca786c1591304e819711236cc4008353f69e
Simonbelete <simonbelete@gmail.com>
2023-03-21 13:23:47 +0300

    fixing datatables lists

--- ilri_ges/breeding_pairs/static/js/breeding_pairs_list.js
+++ ilri_ges/breeding_pairs/static/js/breeding_pairs_list.js
@@ -1,75 +1,58 @@
-requirejs(
-  [
-    "jquery",
-    "datatables.net",
-    "datatables.net-bs4",
-    "datatables.net-responsive",
-    "datatablesResponsiveBs4",
-    "datatables.net-buttons",
-    "datatablesButtonsBs4",
-    "jszip",
-    "pdfmake",
-    "pdfmake_fonts",
-    "datatables_buttons_html5",
-    "datatables_buttons_print",
-    "datatables_buttons_colVis",
-  ],
-  function ($, DataTable, dbs4) {
-    "use strict";
+requirejs(["jquery", "datatables"], function ($, DataTable, dbs4) {
+  "use strict";
 
-    var selector = $("#breeding_pairs_data_table_list");
+  var selector = $("#breeding_pairs_data_table_list");
 
-    var columns = [
-      { data: "id", visible: false },
-      { data: "date" },
-      { data: "sire.tag" },
-      { data: "dam.tag" },
-      { data: "children_count" },
-      { data: "created_at" },
-      {
-        data: null,
-        className: "dt-center editor-edit",
-        defaultContent:
-          '<button type="button" class="btn btn-success">View / Edit</button>',
-        orderable: false,
+  var columns = [
+    { data: "id", visible: false },
+    { data: "date" },
+    { data: "sire.tag" },
+    { data: "dam.tag" },
+    { data: "children_count" },
+    { data: "created_at" },
+    {
+      data: null,
+      className: "dt-center editor-edit",
+      defaultContent:
+        '<button type="button" class="btn btn-success">View / Edit</button>',
+      orderable: false,
+    },
+  ];
+  var table = selector.DataTable({
+    processing: true,
+    serverSide: true,
+    responsive: true,
+    autoWidth: false,
+    lengthChange: true,
+    buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+    ajax: {
+      url: "/api/breeding-pairs/",
+      dataSrc: function (json) {
+        json["data"] = json["results"];
+        json["recordsTotal"] = json["count"];
+        json["recordsFiltered"] = json["count"];
+        return json.data;
       },
-    ];
-    var table = selector.DataTable({
-      processing: true,
-      serverSide: true,
-      responsive: true,
-      autoWidth: false,
-      lengthChange: true,
-      buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
-      ajax: {
-        url: "/api/breeding-pairs/",
-        dataSrc: function (json) {
-          json["data"] = json["results"];
-          json["recordsTotal"] = json["count"];
-          json["recordsFiltered"] = json["count"];
-          return json.data;
-        },
-        data: function (d) {
-          d.search = d.search.value;
-          d.offset = d.start;
-          d.limit = d.length;
-          var sign = d.order[0].dir == "asc" ? "+" : "-";
-          d.ordering = sign + columns[d.order[0].column].data;
+      data: function (d) {
+        d.search = d.search.value;
+        d.offset = d.start;
+        d.limit = d.length;
+        var sign = d.order[0].dir == "asc" ? "+" : "-";
+        d.ordering = sign + columns[d.order[0].column].data;
 
-          d.columns = [];
-          d.order = [];
-          delete d.length;
-          delete d.draw;
-        },
+        d.columns = [];
+        d.order = [];
+        delete d.length;
+        delete d.draw;
       },
-      columns: columns,
-    });
+    },
+    columns: columns,
+  });
 
-    // Edit record
-    selector.on("click", "td.editor-edit, tr td ul .editor-edit", function (e) {
-      e.preventDefault();
-      var data = table.row(this).data();
-      window.location.href = "/breeding-pairs/" + data.id;
-    });
-  }
-);
+  // Edit record
+  selector.on("click", "td.editor-edit, tr td ul .editor-edit", function (e) {
+    e.preventDefault();
+    var data = table.row(this).data();
+    window.location.href = "/breeding-pairs/" + data.id;
+  });
+});
