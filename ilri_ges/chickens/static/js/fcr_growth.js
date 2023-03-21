cd58fa9e49412ec3e471146d7021d136cbfc5507
Simonbelete <simonbelete@gmail.com>
2023-03-21 13:21:23 +0300

    fixing fcr charts

--- ilri_ges/chickens/static/js/fcr_growth.js
+++ ilri_ges/chickens/static/js/fcr_growth.js
@@ -1,81 +1,78 @@
-requirejs(
-  ["jquery", "datatables", "lodash", "chartjs"],
-  function ($, DataTable, Chart) {
-    "use strict";
+requirejs(["jquery", "datatables", "chartjs"], function ($, DataTable, Chart) {
+  "use strict";
 
-    var selector = $("#fcrg_data_list_table");
+  var selector = $("#fcrg_data_list_table");
 
-    var columns = [
-      { data: "week" },
-      { data: "weight_gain" },
-      { data: "feed_weight" },
-      { data: "fcr" },
-    ];
+  var columns = [
+    { data: "week" },
+    { data: "weight_gain" },
+    { data: "feed_weight" },
+    { data: "fcr" },
+  ];
 
-    var chartId = $("#fcrg_chart").get(0).getContext("2d");
-    var chart = new Chart(chartId, {
-      type: "line",
-      data: {
-        labels: [],
-        datasets: [],
+  var chartId = $("#fcrg_chart").get(0).getContext("2d");
+  var chart = new Chart(chartId, {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [],
+    },
+    options: {
+      title: {
+        display: true,
+        text: "FCR - Growth",
       },
-      options: {
-        title: {
-          display: true,
-          text: "FCR - Growth",
-        },
+    },
+  });
+
+  var table = selector.DataTable({
+    responsive: true,
+    autoWidth: false,
+    ordering: false,
+    dom: "lrt",
+    bPaginate: false,
+    processing: true,
+    serverSide: true,
+    ajax: {
+      url: "/api/chickens/" + selector.data("id") + "/fcr/growth",
+      dataSrc: function (json) {
+        // For Chartjs
+        var datasets = {
+          data: [],
+          label: "",
+          borderColor: "#c45850",
+          fill: true,
+        };
+        var labels = [];
+        for (var i = 0; i < json["results"].length; i++) {
+          labels.push(json["results"][i]["week"]);
+          datasets.data.push(json["results"][i]["fcr"]);
+        }
+        chart.data.labels = labels;
+        chart.data.datasets[0] = datasets;
+        chart.update();
+
+        json["data"] = json["results"];
+        json["recordsTotal"] = json["count"];
+        json["recordsFiltered"] = json["count"];
+        return json.data;
       },
-    });
+      data: function (d) {
+        d.start_week = $("#fcrg_start_week").val();
+        d.end_week = $("#fcrg_end_week").val();
 
-    var table = selector.DataTable({
-      responsive: true,
-      autoWidth: false,
-      ordering: false,
-      dom: "lrt",
-      bPaginate: false,
-      processing: true,
-      serverSide: true,
-      ajax: {
-        url: "/api/chickens/" + selector.data("id") + "/fcr/growth",
-        dataSrc: function (json) {
-          // For Chartjs
-          var datasets = {
-            data: [],
-            label: "",
-            borderColor: "#c45850",
-            fill: true,
-          };
-          var labels = [];
-          for (var i = 0; i < json["results"].length; i++) {
-            labels.push(json["results"][i]["week"]);
-            datasets.data.push(json["results"][i]["fcr"]);
-          }
-          chart.data.labels = labels;
-          chart.data.datasets[0] = datasets;
-          chart.update();
-
-          json["data"] = json["results"];
-          json["recordsTotal"] = json["count"];
-          json["recordsFiltered"] = json["count"];
-          return json.data;
-        },
-        data: function (d) {
-          d.start_week = $("#fcrg_start_week").val();
-          d.end_week = $("#fcrg_end_week").val();
-
-          d.columns = [];
-          d.order = [];
-          delete d.start;
-          delete d.search;
-          delete d.length;
-          delete d.draw;
-        },
+        d.columns = [];
+        d.order = [];
+        delete d.start;
+        delete d.search;
+        delete d.length;
+        delete d.draw;
       },
-      columns: columns,
-    });
+    },
+    columns: columns,
+  });
 
-    $("#fcrg_apply").click(function () {
-      table.ajax.reload(null, false);
-    });
-  }
-);
+  $("#fcrg_apply").click(function () {
+    table.ajax.reload(null, false);
+  });
+});
