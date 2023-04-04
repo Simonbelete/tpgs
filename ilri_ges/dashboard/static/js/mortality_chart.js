a7af363e708db5dee53c2061446e184eab1232d6
Simonbelete <simonbelete@gmail.com>
2023-04-04 15:41:05 +0300

    mortality number chart

---
+++ ilri_ges/dashboard/static/js/mortality_chart.js
@@ -0,0 +1,51 @@
+requirejs(
+  ["jquery", "datatables", "lodash", "chartjs4"],
+  function ($, DataTable, _, Chart) {
+    "use strict";
+
+    var chartId = $("#mortaility-chart-canvas").get(0).getContext("2d");
+    var chart = new Chart(chartId, {
+      type: "line",
+      data: {
+        labels: [],
+        datasets: [],
+      },
+      options: {
+        title: {
+          display: true,
+          text: "Weights",
+        },
+        // backgroundColor: "#F5DEB3",
+      },
+      plugins: [
+        {
+          beforeDraw: (chart, args, options) => {
+            const { ctx } = chart;
+            ctx.save();
+            ctx.globalCompositeOperation = "destination-over";
+            ctx.fillStyle = "#fff";
+            ctx.fillRect(0, 0, chart.width, chart.height);
+            ctx.restore();
+          },
+        },
+      ],
+    });
+
+    $.getJSON("/api/chickens-mortality/", {}).done(function (response) {
+      console.log(response);
+      chart.data.labels = response.chartjs.labels;
+      chart.data.datasets[0] = {
+        data: response.chartjs.data,
+        label: "Mortality",
+        backgroundColor: "rgba(60,141,188,0.9)",
+        borderColor: "rgba(60,141,188,0.8)",
+        pointRadius: false,
+        pointColor: "#3b8bba",
+        pointStrokeColor: "rgba(60,141,188,1)",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(60,141,188,1)",
+      };
+      chart.update();
+    });
+  }
+);
