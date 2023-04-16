38db74fc22ca7831e8dd002f9c70eddbe14bf1b7
Simonbelete <simonbelete@gmail.com>
2023-04-16 13:17:47 +0300

    web components setup

---
+++ components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from "react";
+import mui from "@mui/material";
+
+const Button = (): ReactElement => {
+  return <mui.Button></mui.Button>;
+};
+
+export default Button;
