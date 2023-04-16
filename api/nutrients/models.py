21bd58149c3aececba828e9f1ecd5f963b11d349
Simonbelete <simonbelete@gmail.com>
2023-04-16 17:57:05 +0300

    nutrients group with fixtures

--- api/nutrients/models.py
+++ api/nutrients/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-from core.models import CoreModel
+from units.models import Unit
 
-class Nutrients(CoreModel):
+class NutrientGroup(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class Nutrient(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=100, unique=True)
+    nutrient_group = models.ForeignKey(
+        NutrientGroup, on_delete=models.SET_NULL, null=True, related_name='nutrients')
+    unit = models.ForeignKey(
+        Unit, on_delete=models.SET_NULL, null=True, related_name='nutrients')
+    min = models.FloatField()
+    max = models.FloatField()
