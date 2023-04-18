1f6f216cfee53b091668b8458017d55df7d15a06
Simonbelete <simonbelete@gmail.com>
2023-04-18 22:42:45 +0300

    adding locale to date time picker

--- ilri_ges/static/js/date_time_picker.js
+++ ilri_ges/static/js/date_time_picker.js
@@ -1,18 +1,15 @@
 requirejs(
   ["jquery", "daterangepicker", "moment"],
   function ($, daterangepicker, moment) {
-    $("#date_time").daterangepicker(
-      {
-        timePicker: true,
-        autoApply: true,
-        singleDatePicker: true,
-        showDropdowns: true,
-        minYear: 1901,
-        maxYear: parseInt(moment().format("YYYY"), 10),
+    $("#date_time").daterangepicker({
+      timePicker: true,
+      singleDatePicker: true,
+      showDropdowns: true,
+      // minYear: 1901,
+      // maxYear: parseInt(moment().format("YYYY"), 10),
+      locale: {
+        format: "M/DD/YYYY hh:mm A",
       },
-      function (start, end, label) {
-        var years = moment().diff(start, "years");
-      }
-    );
+    });
   }
 );
