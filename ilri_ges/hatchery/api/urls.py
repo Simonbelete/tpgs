6d190f7bc72f0dfb2db83e0c83c6c47f8ebeb754
Simonbelete <simonbelete@gmail.com>
2023-04-18 21:56:13 +0300

    rest api for incubation and hatchery

--- ilri_ges/hatchery/api/urls.py
+++ ilri_ges/hatchery/api/urls.py
@@ -10,6 +10,10 @@ router.register(r'incubation', views.IncubationViewSet,
                 basename='api_incubation')
 router.register(r'candling', views.CandlingViewSet,
                 basename='api_candling')
+router.register(r'hatchery/(?P<id>.+)/incubation', views.HatcheryIncubation,
+                basename='api_hatchery_incubation')
+router.register(r'hatchery/(?P<id>.+)/candling', views.HatcheryCandling,
+                basename='api_hatchery_candling')
 
 urlpatterns = [
     path('', include(router.urls)),
--- ilri_ges/hatchery/api/views.py
+++ ilri_ges/hatchery/api/views.py
@@ -10,19 +10,46 @@ from django.db.models import F
 from .. import models
 from . import serializers
 
+
 class HatcheryViewSet(viewsets.ModelViewSet):
     queryset = models.Hatchery.objects.all()
-    serializer_class  = serializers.HatcherySerializer_GET
+    serializer_class = serializers.HatcherySerializer_GET
     ordering_fields = '__all__'
 
 
 class IncubationViewSet(viewsets.ModelViewSet):
     queryset = models.Incubation.objects.all()
-    serializer_class  = serializers.IncubationSerializer_GET
+    serializer_class = serializers.IncubationSerializer_GET
     ordering_fields = '__all__'
 
 
 class CandlingViewSet(viewsets.ModelViewSet):
     queryset = models.Candling.objects.all()
-    serializer_class  = serializers.CandlingSerializer_GET
+    serializer_class = serializers.CandlingSerializer_GET
     ordering_fields = '__all__'
+
+
+class HatcheryIncubation(viewsets.ModelViewSet):
+    queryset = models.Incubation.objects.all()
+    serializer_class = serializers.IncubationSerializer_GET
+
+    def list(self, request, *args, **kwargs):
+        id = self.kwargs['id']
+        queryset = self.filter_queryset(self.get_queryset().filter(
+            hatchery=id).order_by('date_time'))
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'results': serializer.data})
+
+
+class HatcheryCandling(viewsets.ModelViewSet):
+    queryset = models.Incubation.objects.all()
+    serializer_class = serializers.IncubationSerializer_GET
+
+    def list(self, request, *args, **kwargs):
+        id = self.kwargs['id']
+        queryset = self.filter_queryset(self.get_queryset().filter(
+            hatchery=id).order_by('date_time'))
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'results': serializer.data})
