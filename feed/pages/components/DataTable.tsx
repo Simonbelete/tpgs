3bb310337cc66730abeb12f8be189b58ea61746f
Simonbelete <simonbelete@gmail.com>
2023-04-25 17:34:07 +0300

    column sum

--- feed/pages/components/DataTable.tsx
+++ feed/pages/components/DataTable.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useRef, useMemo } from "react";
+import React, { ReactElement, useRef, useMemo, useEffect } from "react";
 import {
   DataEditor,
   EditableGridCell,
@@ -6,33 +6,63 @@ import {
   GridCellKind,
   GridColumn,
   Item,
+  GridColumnIcon,
 } from "@glideapps/glide-data-grid";
 import { data } from "autoprefixer";
+import _ from "lodash";
 
 interface Ingredient {
   name: string;
+  qty: string;
+  price: string;
   cp: string;
+  min: string;
+  max: string;
 }
 
 const DataTable = (): ReactElement => {
   const columns = useMemo<GridColumn[]>(() => {
     return [
       { title: "Name", id: "name" },
-      { title: "CP%", id: "cp" },
+      { title: "%", id: "qty", icon: GridColumnIcon.HeaderNumber },
+      { title: "Price/Unit", id: "price", icon: GridColumnIcon.HeaderNumber },
+      { title: "CP%", id: "cp", icon: GridColumnIcon.HeaderNumber },
+      { title: "Min%", id: "min", icon: GridColumnIcon.HeaderNumber },
+      { title: "Max%", id: "cp", icon: GridColumnIcon.HeaderNumber },
     ];
   }, []);
   const dataRef = useRef([
     {
       name: "Maze",
-      cp: "23",
+      qty: "",
+      price: "",
+      cp: "10",
+      min: "",
+      max: "",
     },
     {
-      name: "Maze",
-      cp: "23",
+      name: "",
+      qty: "",
+      price: "",
+      cp: "",
+      min: "",
+      max: "",
     },
     {
-      name: "Maze",
-      cp: "23",
+      name: "Recipe",
+      qty: "0",
+      price: "0",
+      cp: "0",
+      min: "0",
+      max: "0",
+    },
+    {
+      name: "Requirement",
+      qty: "10",
+      price: "0",
+      cp: "0",
+      min: "0",
+      max: "0",
     },
   ]);
 
@@ -40,7 +70,14 @@ const DataTable = (): ReactElement => {
     const [col, row] = cell;
     const dataRow = dataRef.current[row];
     // dumb but simple way to do this
-    const indexes: (keyof Ingredient)[] = ["name", "cp"];
+    const indexes: (keyof Ingredient)[] = [
+      "name",
+      "qty",
+      "price",
+      "cp",
+      "min",
+      "max",
+    ];
     const d = dataRow[indexes[col]];
     return {
       kind: GridCellKind.Text,
@@ -57,13 +94,49 @@ const DataTable = (): ReactElement => {
         return;
       }
 
-      const indexes: (keyof Ingredient)[] = ["name", "cp"];
+      const indexes: (keyof Ingredient)[] = [
+        "name",
+        "qty",
+        "price",
+        "cp",
+        "min",
+        "max",
+      ];
       const [col, row] = cell;
       const key = indexes[col];
+
+      // Set value change
       dataRef.current[row][key] = newValue.data;
+
+      let col_values = new Map();
+
+      // const's
+      const last_index = dataRef.current.length - 1;
+      const recipe_index = last_index - 1;
+
+      console.log(dataRef.current);
+
+      // Calculate Each Column indexes sum & values
+      for (let i = 1; i < indexes.length; i++) {
+        const col_key = indexes[i];
+        let vals: any = _.map(
+          dataRef.current.slice(0, recipe_index),
+          (o: Ingredient) => Number(o[col_key])
+        );
+
+        console.log("----");
+        console.log(vals);
+
+        // col_values.set(indexes[i], vals);
+
+        // Set sum
+        dataRef.current[recipe_index][col_key] = String(_.sum(vals));
+      }
     },
     []
   );
+
+  useEffect(() => {}, []);
 
   return (
     <DataEditor
