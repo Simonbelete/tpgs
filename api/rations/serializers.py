0c34dc75eae48e5dda71dd2f42d2104aac6a7b0b
Simonbelete <simonbelete@gmail.com>
2023-05-01 14:13:22 +0300

    ration ingredient creation from rest api

--- api/rations/serializers.py
+++ api/rations/serializers.py
@@ -7,14 +7,23 @@ class RationIngredientSerializer_GET(serializers.ModelSerializer):
         model = models.RationIngredient
         fields = '__all__'
 
-    def create(self, validated_data):
-        ingredients = validated_data.pop('ingredients', None)
-        instance = super().create(validated_data)
-        for ingredient in ingredients:
-            ing = models.RationIngredient(ration=instance, **ingredient)
-            ing.save()
 
-        return instance
+class RationIngredientSerializer_POST(serializers.ModelSerializer):
+    class Meta:
+        model = models.RationIngredient
+        fields = ["ingredient_type",
+                  "dm",
+                  "me",
+                  "cp",
+                  "lys",
+                  "meth",
+                  "mc",
+                  "ee",
+                  "cf",
+                  "ca",
+                  "p",
+                  "ratio_min",
+                  "ratio_max"]
 
 
 class RationSerializer_GET(serializers.ModelSerializer):
@@ -23,3 +32,22 @@ class RationSerializer_GET(serializers.ModelSerializer):
     class Meta:
         model = models.Ration
         fields = ['name', 'ingredients']
+
+
+class RationSerializer_POST(serializers.ModelSerializer):
+    ingredients = RationIngredientSerializer_POST(many=True)
+
+    class Meta:
+        model = models.Ration
+        fields = ['name', 'ingredients']
+
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients', None)
+        instance = super().create(validated_data)
+        print('----------------')
+        print(instance)
+        for ingredient in ingredients:
+            ing = models.RationIngredient(ration=instance, **ingredient)
+            ing.save()
+
+        return instance
--- api/rations/views.py
+++ api/rations/views.py
@@ -8,7 +8,17 @@ class RationViewSet(viewsets.ModelViewSet):
     queryset = models.Ration.objects.all()
     serializer_class = serializers.RationSerializer_GET
 
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return serializers.RationSerializer_GET
+        return serializers.RationSerializer_POST
+
 
 class RationIngredientViewSet(viewsets.ModelViewSet):
     queryset = models.RationIngredient.objects.all()
     serializer_class = serializers.RationIngredientSerializer_GET
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return serializers.RationIngredientSerializer_GET
+        return serializers.RationIngredientSerializer_POST
