adcd8382c320ab737d095f897a1493ca6f713f8d
Simonbelete <simonbelete@gmail.com>
2023-05-12 09:36:59 +0300

    portal: adding fav icon

--- portal/components/menus/PrimaryMenu.tsx
+++ portal/components/menus/PrimaryMenu.tsx
@@ -1,71 +1,12 @@
 import React from "react";
-import {
-  Button,
-  Menu,
-  MenuItem,
-  Popper,
-  Paper,
-  Grow,
-  ClickAwayListener,
-  MenuList,
-  Box,
-  Stack,
-} from "@mui/material";
+import { Button, Box, Stack } from "@mui/material";
+import AppsDropdownMenu from "./AppsDropdownMenu";
 
 const PrimaryMenu = () => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
     <Box sx={{ display: { xs: "none", sm: "block" } }}>
       <Stack direction="row" spacing={2}>
-        <Button
-          id="basic-button"
-          aria-controls={open ? "basic-menu" : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? "true" : undefined}
-          onClick={handleClick}
-        >
-          Dashboard
-        </Button>
-        <Popper
-          open={open}
-          anchorEl={anchorEl}
-          role={undefined}
-          placement="bottom-start"
-          transition
-          disablePortal
-        >
-          {({ TransitionProps, placement }) => (
-            <Grow
-              {...TransitionProps}
-              style={{
-                transformOrigin:
-                  placement === "bottom-start" ? "left top" : "left bottom",
-              }}
-            >
-              <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList
-                    autoFocusItem={open}
-                    id="composition-menu"
-                    aria-labelledby="composition-button"
-                  >
-                    <MenuItem onClick={handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={handleClose}>My account</MenuItem>
-                    <MenuItem onClick={handleClose}>Logout</MenuItem>
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
+        <AppsDropdownMenu />
         <Button>Login</Button>
         <Button variant="outlined">Sign Up</Button>
       </Stack>
