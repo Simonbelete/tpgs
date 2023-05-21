f93ba3c6e103a46384a9222535437d886fa76776
Simonbelete <simonbelete@gmail.com>
2023-05-21 20:10:01 +0300

    ne: data listing

--- ne/app/courses/page.tsx
+++ ne/app/courses/page.tsx
@@ -1,1 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { PrismaClient } from "@prisma/client";
+import Course from "@/interfaces/Course";
+
+async function getData() {
+  const prisma = new PrismaClient();
+  const result = await prisma.course.findMany();
+  return result;
+}
+
+export default async function Page() {
+  const data: Course[] = await getData();
+
+  return (
+    <section>
+      <h1>Courses List</h1>
+      {data.map((e, key: any) => (
+        <p key={key}>{e.title_am}</p>
+      ))}
+    </section>
+  );
+}
---
+++ ne/interfaces/Course.ts
@@ -0,0 +1,15 @@
+export default interface Course {
+  id: number;
+  title_en: string;
+  title_am: string;
+  title_sw: string;
+
+  coverImage: string;
+  coverImage_en: string;
+  coverImage_am: string;
+  coverImage_sw: string;
+
+  content_en: string;
+  content_am: string;
+  content_sw: string;
+}
