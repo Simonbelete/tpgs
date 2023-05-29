58a3e37d44d0f7465a631ac0b58a1743a86afaa1
Simonbelete <simonbelete@gmail.com>
2023-05-29 10:44:57 +0300

    changing static url from env

--- ilri_ges/ilri_ges/settings.py
+++ ilri_ges/ilri_ges/settings.py
@@ -166,9 +166,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = 'ilri_ges/static'
+STATIC_ROOT = env('STATIC_ROOT')
 
-STATIC_URL = '/static-serve/'
+STATIC_URL = env('STATIC_URL')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
