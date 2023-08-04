1ea1e5ef4ab34625234f9a59ee1e502b2b61a7b2
Simonbelete <simonbelete@gmail.com>
2023-08-05 00:23:18 +0300

    addding flock and house to chickens

--- api/chickens/models.py
+++ api/chickens/models.py
@@ -3,6 +3,8 @@ from simple_history.models import HistoricalRecords
 
 from core.models import CoreModel
 from units.models import Unit
+from houses.models import House
+from flocks.models import Flock
 
 
 class Chicken(CoreModel):
@@ -18,6 +20,11 @@ class Chicken(CoreModel):
         'self', models.SET_NULL, blank=True, null=True, limit_choices_to={'sex': 'M'}, related_name='children_of_sire')
     dam = models.ForeignKey(
         'self', models.SET_NULL, blank=True, null=True, limit_choices_to={'sex': 'F'}, related_name='children_of_dam')
+    flock = models.ForeignKey(
+        Flock, on_delete=models.SET_NULL, null=True, blank=True, related_name='chickens')
+    house = models.ForeignKey(
+        House, on_delete=models.SET_NULL, null=True, blank=True, related_name='chickens')
+    pen = models.CharField(max_length=250, null=True, blank=True)
     history = HistoricalRecords()
 
     def __str__(self):
