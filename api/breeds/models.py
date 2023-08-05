4b7f318ffec300e8ed3e11b670eb127f39e60764
Simonbelete <simonbelete@gmail.com>
2023-08-05 12:54:01 +0300

    hhep and hdep guid model

--- api/breeds/models.py
+++ api/breeds/models.py
@@ -2,12 +2,45 @@ from django.db import models
 from simple_history.models import HistoricalRecords
 
 from core.models import CoreModel
+from core.validators import WEEK_VALIDATOR, PERCENTAGE_VALIDATOR
 
 
 class Breed(CoreModel):
     name = models.CharField(max_length=250, unique=True)
     color = models.CharField(max_length=10, null=True, blank=True)
+    hdep_desirable_avg = models.PositiveIntegerField(
+        validators=PERCENTAGE_VALIDATOR, null=True, blank=True)
+    hhep_desirable_avg = models.PositiveIntegerField(
+        validators=PERCENTAGE_VALIDATOR, null=True, blank=True)
     history = HistoricalRecords()
 
     def __str__(self):
         return self.name
+
+
+class BreedHDEPGuid(CoreModel):
+    """Hen-Day Egg Production (HDEP)
+    """
+    breed = models.ForeignKey(
+        Breed, on_delete=models.CASCADE)
+    week = models.PositiveIntegerField(validators=WEEK_VALIDATOR)
+    hdep = models.PositiveIntegerField(
+        validators=PERCENTAGE_VALIDATOR, default=0)
+
+
+class BreedHHEPGuid(CoreModel):
+    """Hen-Housed Egg Production (HHEP)
+    """
+    breed = models.ForeignKey(
+        Breed, on_delete=models.CASCADE)
+    week = models.PositiveIntegerField(validators=WEEK_VALIDATOR)
+    hhep = models.PositiveIntegerField(
+        validators=PERCENTAGE_VALIDATOR, default=0)
+
+
+class BreedEggGuid(CoreModel):
+    pass
+
+
+class BreedWeightGuid(CoreModel):
+    pass
--- api/core/validators.py
+++ api/core/validators.py
@@ -1,3 +1,4 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 PERCENTAGE_VALIDATOR = [MinValueValidator(0), MaxValueValidator(100)]
+WEEK_VALIDATOR = [MinValueValidator(0)]
