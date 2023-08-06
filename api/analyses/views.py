97487301830ca4aa9ca75380c69a86a0950c01bc
Simonbelete <simonbelete@gmail.com>
2023-08-06 11:38:39 +0300

    fixing hdep calcuation

--- api/analyses/views.py
+++ api/analyses/views.py
@@ -4,11 +4,13 @@ import django_filters
 from rest_framework.response import Response
 from django.db.models import Q
 from django.db.models import Count, Sum, Avg, F
+from django_tenants.utils import tenant_context
 
 from . import models
 from . import serializers
 from eggs.models import Egg
 from flocks.models import Flock, FlockReduction, FlockAccusation
+from farms.models import Farm
 
 
 class DirectoryListFilter(django_filters.FilterSet):
@@ -38,6 +40,7 @@ class HDEPViewSet(viewsets.ViewSet):
         start_week = int(request.GET.get('start_week', 0))
         end_week = int(request.GET.get('end_week', 0))
         flock_id = kwargs['flock_id']
+        farm_id = kwargs['farm_id']
         eggs = self.get_query()
         if (kwargs['farm_id'] == 'all'):
             return Response({
@@ -45,29 +48,42 @@ class HDEPViewSet(viewsets.ViewSet):
                     'farm can not be all'
                 ]
             })
-        if (kwargs['flock_id'] != 'all'):
-            eggs = eggs.filter(Q(flock=kwargs['flock_id']) | Q(
-                chicken__flock=kwargs['flock_id']))
-        if (kwargs['house_id'] != 'all'):
-            eggs = eggs.filter(chicken__house=kwargs['house_id'])
+        farm = Farm.objects.get(pk=farm_id)
 
-        results = []
-        for week in range(start_week, end_week + 1):
-            weekly_no_eggs = eggs.filter(week=week).aggregate(
-                sum=Sum('eggs'))['sum'] or 0
-            flock_accusation = FlockAccusation.objects.filter(flock=flock_id)
-            total_accusation = [
-                x.total_chickens for x in flock_accusation.iterator() if x.accusation_week <= week]
-            flock_reduction = FlockReduction.objects.filter(flock=flock_id)
-            total_reduction = [
-                x.total_chickens for x in flock_reduction.iterator() if x.accusation_week <= week]
-            alive_chickens = total_accusation - total_reduction
-            hdep = weekly_no_eggs / alive_chickens * 100
-            results.append({
-                'week': week,
-                'hdep': hdep,
-                'accusation': total_accusation,
-                'reduction': total_reduction,
-                'no_eggs': weekly_no_eggs
-            })
-        return Response({'results': results})
+        with tenant_context(farm):
+            if (kwargs['flock_id'] != 'all'):
+                eggs = eggs.filter(Q(flock=kwargs['flock_id']) | Q(
+                    chicken__flock=kwargs['flock_id']))
+            if (kwargs['house_id'] != 'all'):
+                eggs = eggs.filter(chicken__house=kwargs['house_id'])
+
+            results = []
+            for week in range(start_week, end_week + 1):
+                weekly_no_eggs = eggs.filter(week=week).aggregate(
+                    sum=Sum('eggs'))['sum'] or 0
+
+                flock_accusation = FlockAccusation.objects.filter(
+                    flock=flock_id)
+                total_accusation = 0
+                for x in flock_accusation.iterator():
+                    if x.accusation_week <= week:
+                        total_accusation += x.total_chickens
+
+                flock_reduction = FlockReduction.objects.filter(flock=flock_id)
+                total_reduction = 0
+                for x in flock_reduction.iterator():
+                    if x.reduction_week <= week:
+                        total_reduction += x.total_chickens
+
+                alive_chickens = total_accusation - total_reduction
+                alive_chickens = 1 if alive_chickens == 0 else alive_chickens
+
+                hdep = weekly_no_eggs / alive_chickens * 100
+                results.append({
+                    'week': week,
+                    'hdep': hdep,
+                    'accusation': total_accusation,
+                    'reduction': total_reduction,
+                    'no_eggs': weekly_no_eggs
+                })
+            return Response({'results': results})
