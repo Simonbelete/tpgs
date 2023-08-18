3ee32429cf22c8d8b9092902dc5940ce813b3b47
Simonbelete <simonbelete@gmail.com>
2023-08-18 20:50:15 +0300

    chp 6 & 7 am

---
+++ nea/lib/i18n/6/am.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:nea/models/course_model.dart';
+import 'package:nea/widgets/bullet.dart';
+import 'package:nea/widgets/course_body_image.dart';
+import 'package:nea/widgets/keymessages.dart';
+import 'package:nea/widgets/objectives.dart';
+import 'package:nea/widgets/paragraph.dart';
+import 'package:nea/widgets/sub_title_text.dart';
+
+Course EN = Course(
+  title: "ሴቶችን ማብቃትና የወንዶች ተሳትፎ",
+  coverImage: "assets/materials/images/4.png",
+  body: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: const [
+      Objectives(title: "ዓላማ፡ ", children: [
+        "ጥሩ የቤተሰብ አመጋገብን በማስተዋወቅ ረገድ የወንዶች ሚና ያላቸውን ግንዛቤ ለማሳደግ እና ሴቶች ጥሩ አመጋገብን ለማግኘት በቤተሰብ ሃብት ዙሪያ ውሳኔ እንዲያደርጉ ለማበረታታት"
+      ]),
+      CourseBodyImage(
+        image: "assets/materials/images/4.png",
+      ),
+      KeyMessages(title: 'ጭብጥ መልዕክቶች', children: [
+        "ባልና ሚስት ያገኙትን ገቢ እንዴት መጠቀም እንደሚችሉ በጋራ መወያየት አለባቸው፡፡",
+        "ባል በቤተሰቡ ውስጥ በቂ የተለያዩ ምግብ መኖሩን ማረጋገጥ አለበት።",
+        "ባል የሚስትን የሥራ ጫና ለመርዳት አስፈላጊ በሚሆንበት ጊዜ መደገፍ አለበት።",
+        "ሚስት ለመስራት በቂ ጊዜ እንድታገኝ ነፃ ጊዜ ሊኖራት ይገባል፡፡",
+        "ባልና ሚስት በጋራ መወያየት እና የቤተሰብ ምግቦችን ማቀድ አለባቸው፡፡",
+        "ባልና ሚስት በጋራ በመወያየት በምግብ አቅርቦት ላይ ክፍተቶችን መለየት አለባቸው፡፡",
+      ]),
+      SubTitleText(text: 'ሴቶችን ማብቃት ለምን ያስፈልጋል?'),
+      Bullet(children: [
+        "ሴቶች በስልጣን እና በውሳኔ ሰጪነት ሚና ዝቅተኛ ውክልና ስላላቸው፡፡",
+        "ሴቶች ለእኩል ሥራ እኩል ያልሆነ ክፍያ ያገኛሉ።",
+        "ለአካላዊ እና/ወሲባዊ ጥቃት ስለሚጋለጡ።",
+        "ተመሳሳይ የስራ እድሎች፣ ትምህርት፣ ስልጠና እና ሙያዊ እድገት እድሎች ስለማያገኙ።",
+        "ሴቶች በኢኮኖሚ የተቸገሩ እና ለንግድ ስራ ለመወዳደር እኩል እድል ስለሌላቸው።",
+        "የሥርዓተ-ፆታ እኩልነት በውሳኔ ሰጪዎች ትኩረት ስላልተሰጠው፡፡",
+      ]),
+      SubTitleText(text: 'እድሎች'),
+      Bullet(children: [
+        "የሴቶችን ጤና መደገፍ የቤተሰቡን ገቢ በማሻሻል እድገትን ያመጣል፡፡",
+        "በሴቶች ላይ የሚደርሱ ጥቃቶችን መፍታት የድርጅቶችን ወጪ ይቀንሳል።",
+        "ለሴቶች ሃላፊነትን መስጠት ምርታማነትን ይጨምራል።",
+        "በሴቶች ባለቤትነት ስር ባሉ የንግድ ሥራዎች ላይ ኢንቨስት ማድረግ ለኢንቨስትመንት ከፍተኛ ትርፍ ያስገኛል።",
+        "የሴቶችን የሸማች ሃይል መጠቀም ብልህ የንግድ ውሳኔ ነው።",
+        "ሴቶችን በአመራር ላይ ማሳደግ ወደ ድርጅታዊ ውጤታማነት ብልጽግና መስመር ይመራል።"
+      ]),
+      Paragraph(
+          title:
+              'ሴቶችን ማብቃት ለዘላቂ ልማት ወሳኝ ነው። ሴቶች በሁሉም ዘርፎች እና በሁሉም የኢኮኖሚ እንቅስቃሴዎች ውስጥ ሙሉ በሙሉ እንዲሳተፉ ማብቃት ለሚከተሉት አስፈላጊ ነው፡፡',
+          body: ""),
+      Bullet(children: [
+        "የኢኮኖሚ እድገትን በማስፋት ጠንካራ ኢኮኖሚዎችን መገንባት።",
+        "ማህበራዊ ልማትን ማሳደግ እና የተረጋጋ እና ፍትሃዊ ማህበረሰብን ማቋቋም።",
+        "ለሴቶች፣ ለወንዶች፣ ለቤተሰብ እና ለማህበረሰብ የህይወት ጥራትን ማሻሻል።",
+        "የንግድ ሥራዎችን እና ግቦችን ያንቀሳቅሱ እና የንግድ ሥራ አፈፃፀምን ማሳደግ።"
+      ])
+    ],
+  ),
+);
---
+++ nea/lib/i18n/7/am.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:nea/models/course_model.dart';
+import 'package:nea/screens/home_screen.dart';
+import 'package:nea/widgets/bullet.dart';
+import 'package:nea/widgets/course_body_image.dart';
+import 'package:nea/widgets/paragraph.dart';
+import 'package:nea/widgets/sub_title_text.dart';
+
+Course EN = Course(
+  coverImage: 'assets/materials/images/image1057.png',
+  title: 'የምግብ ዝግጅት የጥንቃቄ መስፈርቶች',
+  body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: const [
+    CourseBodyImage(image: 'assets/materials/images/image1057.png'),
+    SubTitleText(text: 'በምግብ ዝግጅት ጊዜ መወሰድ ያለባቸው የንጽህና ጥንቃቄዎች'),
+    Paragraph(
+        title: '',
+        body:
+            'ጤናማ እና የተመጣጠነ ምግብ ለጤና አስፈላጊ እንደሆነ ሁሉ የምግብ ንጽህናን መጠበቅም እንደ መርህ መከተል አለብን፡፡ የሚከተሉትን ተግባራዊ አድርጉ'),
+    Bullet(children: [
+      'የማዕድ ቤት እቃዎች እና ሌሎች የመመገቢያ እቃዎች ንጹህ መሆናቸውን ያረጋግጡ፡፡',
+      'ምግብ የሚያበስሉበት ቦታ ከቆሻሻ ንጹህ መሆኑን፡ ከአይጦች፡ ከእንሽላሊቶች፡ ከበረሮዎች የጸዳ መሆኑን ያረጋግጡ፡፡',
+      'የመጥበሻ ዘይት ከሁለት ጊዜ በላይ አትጠቀሙ፡፡ ሁሌም አዲስ ዘይት ተጠቀሙ፡፡',
+      'አርቴፊሻል ማጣፈጫዎችን ከመጠቀም በመቆጠብ ጨው፡ ስኳር፡ እና በርበሬ ተጠቀሙ፡፡'
+    ]),
+    SubTitleText(
+        text:
+            '1. እጃችሁን ዘወትር በውሃና በሳሙና ታጠቡ፡፡ አትክልት ከማዘጋጀቱ በፊት በዉሃና በአጃክስ እጠቡት፡፡'),
+    CourseBodyImage(image: 'assets/materials/images/image1060.png'),
+    SubTitleText(
+        text: '2. የማድ ቤት እቃዎች በንጽህና በመያዝ የበሰሉና ያልበሰሉ ምግቦችን በመለየት አስቀምጡ፡፡'),
+    CourseBodyImage(image: 'assets/materials/images/image1061.png'),
+    SubTitleText(
+        text:
+            '3. ምግባችሁ በአግባቡ መብሰሉን አረጋግጡ፡፡ በተለይ የእንስሳት ተዋጽኦ ምግቦች በአግባቡ መብሰል አለባቸው፡'),
+    CourseBodyImage(image: 'assets/materials/images/image1062.png'),
+    SubTitleText(text: '4. ንጹህ ዉሃ፡ ትኩስ እና ያልተበላሸ የምግብ ጥሬ ዕቃ ተጠቀሙ፡፡'),
+    CourseBodyImage(image: 'assets/materials/images/image1063.png'),
+    SubTitleText(text: '5. የበሰለን ምግብ በንጹህ ቦታ ያስቀምጡ፡፡ ከተቻለ በፍሪጅ ውስጥ ያስቀምጡ፡፡'),
+    CourseBodyImage(image: 'assets/materials/images/image1064.png'),
+    SubTitleText(text: 'ምግብ በምናበስልብት ጊዜ መዉስድ ያለብን ጥንቃቄ'),
+    Paragraph(
+        title: '',
+        body:
+            'በቤት ውስጥ እና ውጭ የምናደርጋቸው እንቅስቃሴዎቻችን ከተለያዩ ቦታዎቸ ለሚመጡ ጀርሞች እና ተሃዋሲያን ተጋላጭ ያደርጉናል፡፡ ይህን ለመከላከል ማድ ቤታችን ከጭስ፡ ከቆሻሻና ከተባይ የጸዳ መሆን አለበት፡፡'),
+    Paragraph(title: 'ምግብ በሚበሰልበት ጊዜ የሚከተሉት ጥንቃቄዎች መወሰድ አለባቸው', body: ''),
+    CourseBodyImage(image: 'assets/materials/images/image1134.png'),
+    Paragraph(
+        title: 'የማብሰያ ልብስ ለብሰዉ ያብስሉ',
+        body: 'ለማብሰያነት የሚጠቀሙበትን ጋዋን ዘወትር በማጠብ ንጹህ ያድርጉ'),
+    CourseBodyImage(image: 'assets/materials/images/image1135.png'),
+    Paragraph(
+        title: 'ጸጉርዎን በመጠቅለያ ይሰሩ',
+        body: 'የምግብ ብክለትን ለመከላከል ጸጉርወትን በመጠቅለያ ይጠቅሉ'),
+    CourseBodyImage(image: 'assets/materials/images/image1133.png'),
+    Paragraph(
+        title: 'እጅዎትን በውሃና በሳሙና ይታጠቡ',
+        body:
+            'ምግብዎትን ከጀርሞች ለመከላከል እጅወትን ከምግብ በፊትና ከምግብ በኋላ ለ 60 ሰከንድ በውሃና በሳሙና ይታጠቡ')
+  ]),
+);
