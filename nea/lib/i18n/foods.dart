66795512286ae10d2fa489aff297b9c9e7961ecd
Simonbelete <simonbelete@gmail.com>
2023-08-19 00:09:02 +0300

    foods translation

--- nea/lib/i18n/foods.dart
+++ nea/lib/i18n/foods.dart
@@ -11,28 +11,37 @@ Map<String, Map<String, Food>> carbohydratesFood = {
   'Banana': {
     'en':
         Food(coverImage: "assets/materials/foods/banana.png", title: "Banana"),
+    'sw': Food(coverImage: "assets/materials/foods/banana.png", title: "Ndizi"),
     'am': Food(coverImage: "assets/materials/foods/banana.png", title: "ሙዝ")
   },
   'Teff-Grain': {
     'en': Food(
         coverImage: "assets/materials/foods/Teff-Grain.jpg", title: "Teff"),
+    'sw': Food(
+        coverImage: "assets/materials/foods/Teff-Grain.jpg", title: "Teff"),
     'am': Food(coverImage: "assets/materials/foods/Teff-Grain.jpg", title: "ጤፍ")
   },
   'Corn': {
     'en': Food(coverImage: "assets/materials/foods/corn.png", title: "Corn"),
+    'sw': Food(coverImage: "assets/materials/foods/corn.png", title: "Mahindi"),
     'am': Food(coverImage: "assets/materials/foods/corn.png", title: "በቆሎ")
   },
   'Bread': {
     'en': Food(coverImage: "assets/materials/foods/bread.png", title: "Bread"),
+    'sw': Food(coverImage: "assets/materials/foods/bread.png", title: "Mkate"),
     'am': Food(coverImage: "assets/materials/foods/bread.png", title: "ዳቦ")
   },
   'Beans': {
     'en': Food(coverImage: "assets/materials/foods/beans.png", title: "Beans"),
+    'sw':
+        Food(coverImage: "assets/materials/foods/beans.png", title: "Maharage"),
     'am': Food(coverImage: "assets/materials/foods/beans.png", title: "ባቄላ")
   },
   'potatoes': {
     'en': Food(
         coverImage: "assets/materials/foods/potatoes.png", title: "Potatoes"),
+    'sw':
+        Food(coverImage: "assets/materials/foods/potatoes.png", title: "Viazi"),
     'am': Food(coverImage: "assets/materials/foods/potatoes.png", title: "ድንች")
   }
 };
@@ -40,38 +49,51 @@ Map<String, Map<String, Food>> carbohydratesFood = {
 Map<String, Map<String, Food>> vitaminsFood = {
   'Carrot': {
     'en': Food(coverImage: "assets/materials/foods/carot.png", title: "Carrot"),
+    'sw': Food(coverImage: "assets/materials/foods/carot.png", title: "Karoti"),
     'am': Food(coverImage: "assets/materials/foods/carot.png", title: "ካሮት"),
   },
   'Tomato': {
     'en':
         Food(coverImage: "assets/materials/foods/tomato.png", title: "Tomato"),
+    'sw':
+        Food(coverImage: "assets/materials/foods/tomato.png", title: "Nyanya"),
     'am': Food(coverImage: "assets/materials/foods/tomato.png", title: "ቲማቲም")
   },
   'Watermelon': {
     'en': Food(
         coverImage: "assets/materials/foods/watermelon.png",
         title: "Watermelon"),
+    'sw': Food(
+        coverImage: "assets/materials/foods/watermelon.png",
+        title: "Tikiti maji"),
     'am':
         Food(coverImage: "assets/materials/foods/watermelon.png", title: "ሐብሐብ")
   },
   'Avocado': {
     'en': Food(
         coverImage: "assets/materials/foods/avocado.png", title: "Avocado"),
+    'sw': Food(
+        coverImage: "assets/materials/foods/avocado.png", title: "Parachichi"),
     'am': Food(coverImage: "assets/materials/foods/avocado.png", title: "አቮካዶ")
   },
   'Banana': {
     'en':
         Food(coverImage: "assets/materials/foods/banana.png", title: "Banana"),
+    'sw': Food(coverImage: "assets/materials/foods/banana.png", title: "Ndizi"),
     'am': Food(coverImage: "assets/materials/foods/banana.png", title: "ሙዝ")
   },
   'Orange': {
     'en':
         Food(coverImage: "assets/materials/foods/orange.png", title: "Orange"),
+    'sw':
+        Food(coverImage: "assets/materials/foods/orange.png", title: "Chungwa"),
     'am': Food(coverImage: "assets/materials/foods/orange.png", title: "ብርቱካን")
   },
   'Pumpkin': {
     'en': Food(
         coverImage: "assets/materials/foods/pumpkin.png", title: "Pumpkin"),
+    'sw': Food(
+        coverImage: "assets/materials/foods/pumpkin.png", title: "Malenge"),
     'am': Food(coverImage: "assets/materials/foods/pumpkin.png", title: "ዱባ")
   }
 };
@@ -104,25 +126,33 @@ Map<String, Map<String, Food>> fatsFood = {
     'en': Food(
         coverImage: "assets/materials/foods/vegetable_oil.png",
         title: "Cooking oil"),
+    'sw': Food(
+        coverImage: "assets/materials/foods/vegetable_oil.png",
+        title: "Mafuta ya kupikia"),
     'am': Food(
         coverImage: "assets/materials/foods/vegetable_oil.png",
         title: "የማብሰያ ዘይት")
   },
   'Egg': {
     'en': Food(coverImage: "assets/materials/foods/egg.png", title: "Egg"),
+    'sw': Food(coverImage: "assets/materials/foods/egg.png", title: "Yai"),
     'am': Food(coverImage: "assets/materials/foods/egg.png", title: "እንቁላል")
   },
   'Avocado': {
     'en': Food(
         coverImage: "assets/materials/foods/avocado.png", title: "Avocado"),
+    'sw': Food(
+        coverImage: "assets/materials/foods/avocado.png", title: "Parachichi"),
     'am': Food(coverImage: "assets/materials/foods/avocado.png", title: "አቮካዶ")
   },
   'Milk': {
     'en': Food(coverImage: "assets/materials/foods/milk.png", title: "Milk"),
+    'sw': Food(coverImage: "assets/materials/foods/milk.png", title: "Maziwa"),
     'am': Food(coverImage: "assets/materials/foods/milk.png", title: "ወተት")
   },
   'Fish': {
     'en': Food(coverImage: "assets/materials/foods/fish.png", title: "Fish"),
+    'sw': Food(coverImage: "assets/materials/foods/fish.png", title: "Samaki"),
     'am': Food(coverImage: "assets/materials/foods/fish.png", title: "ዓሳ")
   }
 };
@@ -130,38 +160,51 @@ Map<String, Map<String, Food>> fatsFood = {
 Map<String, Map<String, Food>> mineralsFood = {
   'Carrot': {
     'en': Food(coverImage: "assets/materials/foods/carot.png", title: "Carrot"),
+    'sw': Food(coverImage: "assets/materials/foods/carot.png", title: "Karoti"),
     'am': Food(coverImage: "assets/materials/foods/carot.png", title: "ካሮት")
   },
   'Tomato': {
     'en':
         Food(coverImage: "assets/materials/foods/tomato.png", title: "Tomato"),
+    'sw':
+        Food(coverImage: "assets/materials/foods/tomato.png", title: "Nyanya"),
     'am': Food(coverImage: "assets/materials/foods/tomato.png", title: "ቲማቲም")
   },
   'Avocado': {
     'en': Food(
         coverImage: "assets/materials/foods/avocado.png", title: "Avocado"),
+    'sw': Food(
+        coverImage: "assets/materials/foods/avocado.png", title: "Parachichi"),
     'am': Food(coverImage: "assets/materials/foods/avocado.png", title: "አቮካዶ")
   },
   'Watermelon': {
     'en': Food(
         coverImage: "assets/materials/foods/watermelon.png",
         title: "Watermelon"),
+    'sw': Food(
+        coverImage: "assets/materials/foods/watermelon.png",
+        title: "Tikiti maji"),
     'am':
         Food(coverImage: "assets/materials/foods/watermelon.png", title: "ሐብሐብ")
   },
   'Banana': {
     'en':
         Food(coverImage: "assets/materials/foods/banana.png", title: "Banana"),
+    'sw': Food(coverImage: "assets/materials/foods/banana.png", title: "Ndizi"),
     'am': Food(coverImage: "assets/materials/foods/banana.png", title: "ሙዝ")
   },
   'Orange': {
     'en':
         Food(coverImage: "assets/materials/foods/orange.png", title: "Orange"),
+    'sw':
+        Food(coverImage: "assets/materials/foods/orange.png", title: "Chungwa"),
     'am': Food(coverImage: "assets/materials/foods/orange.png", title: "ብርቱካን")
   },
   'Pumpkin': {
     'en': Food(
         coverImage: "assets/materials/foods/pumpkin.png", title: "Pumpkin"),
+    'sw': Food(
+        coverImage: "assets/materials/foods/pumpkin.png", title: "Malenge"),
     'am': Food(coverImage: "assets/materials/foods/pumpkin.png", title: "ዱባ")
   }
 };
