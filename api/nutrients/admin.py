e686dba8c688a840ff539123a97be0198b86272c
Simonbelete <simonbelete@gmail.com>
2023-08-20 22:19:05 +0300

    nutrients import resources with foreignkey model

--- api/nutrients/admin.py
+++ api/nutrients/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from import_export import resources
+from import_export import resources, fields, widgets
 from simple_history.admin import SimpleHistoryAdmin
 
 from . import models
+from units.models import Unit
 
 
 class NutrientGroupResource(resources.ModelResource):
@@ -12,10 +13,22 @@ class NutrientGroupResource(resources.ModelResource):
 
 
 class NutrientResource(resources.ModelResource):
+    id = fields.Field(attribute='id', column_name='ID')
+    code = fields.Field(attribute='code', column_name='Code')
+    name = fields.Field(attribute='name', column_name='Name')
+    abbreviation = fields.Field(
+        attribute='abbreviation', column_name='ABBREVIATION')
+    unit = fields.Field(attribute='unit', column_name='Unit',
+                        widget=widgets.ForeignKeyWidget(Unit, field='name'))
+    nutrient_group = fields.Field(attribute='nutrient_group',
+                                  column_name='Nutrient Group', widget=widgets.ForeignKeyWidget(models.NutrientGroup, field='name'))
+    description = fields.Field(
+        attribute='description', column_name='Description')
+
     class Meta:
         model = models.Nutrient
-        fields = ['id', 'code', 'name', 'abbreviation',
-                  'unit__name', 'nutrient_group__name', 'description']
+        skip_unchanged = True
+        report_skipped = False
 
 
 admin.site.register(models.Nutrient, SimpleHistoryAdmin)
