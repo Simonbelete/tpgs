aa16429788b437d7781266b52ace015a50a8f281
Simonbelete <simonbelete@gmail.com>
2023-09-13 03:25:27 +0300

    mapbox fill width

--- portal/features/dashboard/farms-heatmap/index.tsx
+++ portal/features/dashboard/farms-heatmap/index.tsx
@@ -2,21 +2,33 @@ import React, { useEffect, useState } from "react";
 import Plot from 'react-plotly.js';
 import { Paper } from "@mui/material";
 import { StatisticsCard } from "@/components";
+import { Layout } from "plotly.js";
 
 const FarmsHeatmap = () => {
   const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
     setData([{
-      type: "choropleth",
+      type: "choroplethmapbox",
       geojson: "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json",
       locations: ["AL"],
       z: [300],
-      zmin: 25, zmax: 280, colorbar: {y: 0, yanchor: "bottom", title: {text: "US states", side: "right"}}
+      // zmin: 25, zmax: 280, colorbar: {y: 0, yanchor: "bottom", title: {text: "US states", side: "right"}}
+      showscale: false
     }])
   }, [])
   
-  const layout = {mapbox: {center: {lon: 38.763611, lat: 9.005401}, zoom: 4}, width: 500, height: 400};
+  const layout: Partial<Layout> = {
+    mapbox: {
+      // style: 'dark',
+      center: {lon: 38, lat: 9}, 
+      zoom: 0.6,
+    },
+    width: 709,
+    height: 500,
+    margin: {t: 0, l: 0, r: 0, b: 0},
+    showlegend: false,
+  }
   
   
   var config = {mapboxAccessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN};
