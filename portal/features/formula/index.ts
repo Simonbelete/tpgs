67aa8e4ad026acb7ee3299c3e1d7b8c5454f12d2
Simonbelete <simonbelete@gmail.com>
2023-10-10 20:49:43 +0300

    removing formula service

--- portal/features/formula/index.ts
+++ portal/features/formula/index.ts
@@ -1,4 +1,3 @@
-export { default as FormulaService } from "./services/formula_service";
 export * from "./formula-dropdown";
 export * from './formulas-list';
 export * from './formula-import-export';
