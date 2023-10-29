99aefbd405b29cd4cc206104cfbe959d6a25e4fe
Simonbelete <simonbelete@gmail.com>
2023-10-29 10:25:56 +0300

    csv import adding validation error

--- api/nutrients/admin.py
+++ api/nutrients/admin.py
@@ -13,23 +13,24 @@ class NutrientGroupResource(resources.ModelResource):
 
 
 class NutrientResource(resources.ModelResource):
-    id = fields.Field(attribute='id', column_name='ID')
-    code = fields.Field(attribute='code', column_name='Code')
-    name = fields.Field(attribute='name', column_name='Name')
-    abbreviation = fields.Field(
-        attribute='abbreviation', column_name='Abbreviation')
-    unit = fields.Field(attribute='unit', column_name='Unit',
+    # id = fields.Field(attribute='id', column_name='ID')
+    # code = fields.Field(attribute='code', column_name='Code')
+    # name = fields.Field(attribute='name', column_name='Name')
+    # abbreviation = fields.Field(
+    #     attribute='abbreviation', column_name='Abbreviation')
+    unit = fields.Field(attribute='unit', column_name='unit',
                         widget=widgets.ForeignKeyWidget(Unit, field='name'))
-    nutrient_group = fields.Field(attribute='nutrient_group',
-                                  column_name='Nutrient Group', widget=widgets.ForeignKeyWidget(models.NutrientGroup, field='name'))
-    description = fields.Field(
-        attribute='description', column_name='Description')
+    # nutrient_group = fields.Field(attribute='nutrient_group',
+    #                               column_name='Nutrient Group', widget=widgets.ForeignKeyWidget(models.NutrientGroup, field='name'))
+    # description = fields.Field(
+    #     attribute='description', column_name='Description')
 
     class Meta:
         model = models.Nutrient
-        skip_unchanged = True
-        report_skipped = True
-        import_id_fields = ['id']
+        # skip_unchanged = True
+        # report_skipped = True
+        # import_id_fields = ['id']
+        fields=['id', 'name', 'abbreviation', 'unit']
 
 
 admin.site.register(models.Nutrient, SimpleHistoryAdmin)
