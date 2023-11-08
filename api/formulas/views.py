fixing formula requirements

api/formulas/views.py
@@ -14,12 +14,21 @@ from django.http import FileResponse
 from rest_framework.exceptions import NotFound
 from django.db import connection
 
+from core.views import (
+    HistoryViewSet,
+    SummaryViewSet,
+    CoreModelViewSet,
+    GenericExportView,
+    GenericImportView
+)
 from . import models
 from . import serializers
+from . import admin
 from ingredients.models import IngredientNutrient
 from nutrients.serializers import NutrientSerializer_GET
 from nutrients.models import Nutrient
 from .formulate import Formulate
+
 
 class FormulaViewSet(viewsets.ModelViewSet):
     queryset = models.Formula.objects.all()
@@ -31,23 +40,43 @@ class FormulaViewSet(viewsets.ModelViewSet):
         return serializers.FormulaSerializer_GET
 
 
-class FormulaRequirementViewSet(viewsets.ModelViewSet):
+# Formula Requirement
+class FormulaRequirementViewSet(CoreModelViewSet):
+    queryset = models.FormulaRequirement.all.all()
     serializer_class = serializers.FormulaRequirementSerializer_GET
 
     def get_queryset(self):
-        try:
-            return models.FormulaRequirement.all.filter(formula=self.kwargs['formula_pk'])
-        except models.FormulaRequirement.DoesNotExist as ex:
-            raise NotFound()
+        if ('formula_pk' in self.kwargs):
+            return self.queryset.filter(formula=self.kwargs['formula_pk'])
+        return self.queryset
 
     def get_serializer_class(self):
-        if self.request.method == 'POST':
+        if self.request.method in ['POST', 'PUT', 'PATCH']:
             return serializers.FormulaRequirementSerializer_POST
-        if self.request.method in ['PUT', 'PATCH']:
-            return serializers.FormulaRequirementSerializer_PATCH
         return serializers.FormulaRequirementSerializer_GET
 
 
+class FormulaRequirementExport(GenericExportView):
+    def get_dataset(self):
+        return admin.FormulaRequirementResource().export()
+
+
+class FormulaRequirementImport(GenericImportView):
+    def get_resource(self):
+        return admin.FormulaRequirementResource()
+
+
+class FormulaRequirementHistoryViewSet(HistoryViewSet):
+    queryset = models.FormulaRequirement.history.all()
+    serializer_class = serializers.FormulaRequirementHistorySerializer
+
+
+class FormulaRequirementSummaryViewSet(SummaryViewSet):
+    def get_query(self):
+        return models.FormulaRequirement.all.get(pk=self.id_pk)
+
+
+# Formula Rations
 class FormulaRationViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.FormulaRationSerializer_GET
 
@@ -75,7 +104,8 @@ class FormulaRationViewSet(viewsets.ModelViewSet):
             models.FormulaRation.objects.create(
                 formula=instance, nutrient=nutrient, value=req['value'])
         return instance
-    
+
+
 class FormulaIngredientViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.FormulaIngredientSerializer_GET
 
@@ -116,6 +146,8 @@ class FormulateViewSet(viewsets.ViewSet):
         return Response({'results': data.data})
 
 # Print Pdf
+
+
 class FormulaPrintPdf(viewsets.ViewSet):
     """Return computed total cost and total nutrients sum
     """
@@ -130,22 +162,26 @@ class FormulaPrintPdf(viewsets.ViewSet):
         formula = self.get_queryset()
 
         buffer = io.BytesIO()
-        doc = SimpleDocTemplate(buffer, rightMargin=72, leftMargin=72, topMargin=72, bottomMargin=72, pagesize=(landscape(A4)))
+        doc = SimpleDocTemplate(buffer, rightMargin=72, leftMargin=72,
+                                topMargin=72, bottomMargin=72, pagesize=(landscape(A4)))
 
         ingredient_ids = formula.ingredients.values_list('id')
         ingredient_ids = list(zip(*ingredient_ids))[0]
-        ingredient_nutrient = IngredientNutrient.objects.filter(ingredient__in=ingredient_ids)
-        abbvers = np.array(list(zip(*ingredient_nutrient.values_list('nutrient__abbreviation').distinct())))
+        ingredient_nutrient = IngredientNutrient.objects.filter(
+            ingredient__in=ingredient_ids)
+        abbvers = np.array(
+            list(zip(*ingredient_nutrient.values_list('nutrient__abbreviation').distinct())))
         abbvers = np.sort(abbvers)
-        
+
         columns = np.array(['Name', '%', 'Weight[Kg]', 'Price[Kg]', 'DM[%]'])
         columns = np.append(columns, abbvers)
         columns = np.append(columns, ['Min[%]', 'Max[%]'])
 
         data = pd.DataFrame([], columns=columns)
-        
+
         for formula_ingredient in models.FormulaIngredient.objects.filter(formula=formula).iterator():
-            ingredient_nutrients_list = IngredientNutrient.objects.filter(ingredient=formula_ingredient.ingredient).values_list('nutrient__abbreviation', 'value')
+            ingredient_nutrients_list = IngredientNutrient.objects.filter(
+                ingredient=formula_ingredient.ingredient).values_list('nutrient__abbreviation', 'value')
             ingredient_nutrients_dict = dict(ingredient_nutrients_list)
             data.loc[-1] = {
                 'Name': formula_ingredient.ingredient.name,
@@ -159,7 +195,8 @@ class FormulaPrintPdf(viewsets.ViewSet):
             }
             data = data.reset_index(drop=True)
 
-        ration_nutrients_dict = dict(models.FormulaRation.objects.filter(formula=formula).values_list('nutrient__abbreviation', 'value'))
+        ration_nutrients_dict = dict(models.FormulaRation.objects.filter(
+            formula=formula).values_list('nutrient__abbreviation', 'value'))
         data.loc[-1] = {
             'Name': 'Ration',
             '%': formula.ration_ratio,
@@ -171,7 +208,8 @@ class FormulaPrintPdf(viewsets.ViewSet):
             **ration_nutrients_dict
         }
 
-        requirement_nutrients_dict = dict(models.FormulaRequirement.objects.filter(formula=formula).values_list('nutrient__abbreviation', 'value'))
+        requirement_nutrients_dict = dict(models.FormulaRequirement.objects.filter(
+            formula=formula).values_list('nutrient__abbreviation', 'value'))
         data.loc[-2] = {
             'Name': 'Requirement',
             '%': formula.desired_ratio,
@@ -184,11 +222,11 @@ class FormulaPrintPdf(viewsets.ViewSet):
         }
 
         data = data.reset_index(drop=True)
-        data.fillna(0,inplace=True)
+        data.fillna(0, inplace=True)
 
         data_2d = np.vstack([columns, data.to_numpy()])
-        t=Table(data_2d.tolist(), style=[
-            ('GRID',(0,0),(-1,-1),0.5,colors.grey),
+        t = Table(data_2d.tolist(), style=[
+            ('GRID', (0, 0), (-1, -1), 0.5, colors.grey),
         ])
 
         doc.build([t])
@@ -201,6 +239,7 @@ class FormulaPrintPdf(viewsets.ViewSet):
 class FormulaIngredientNutrients(viewsets.ViewSet):
     """Eacth ingredient's nutrient contribution with the requried nutrient
     """
+
     def get_queryset(self):
         try:
             return models.FormulaIngredient.all.get(formula=self.kwargs['formula_pk'], pk=self.kwargs['ingredient_pk'])
@@ -210,10 +249,12 @@ class FormulaIngredientNutrients(viewsets.ViewSet):
     def list(self, request, formula_pk=None, ingredient_pk=None):
         data = []
         formula_ingredient = self.get_queryset()
-        formula_requirement = models.FormulaRequirement.objects.filter(formula=formula_pk)
+        formula_requirement = models.FormulaRequirement.objects.filter(
+            formula=formula_pk)
         for ingredient_nutrient in IngredientNutrient.objects.filter(
                 ingredient=formula_ingredient.ingredient).iterator():
-            nutrient_req = formula_requirement.filter(nutrient=ingredient_nutrient.nutrient)
+            nutrient_req = formula_requirement.filter(
+                nutrient=ingredient_nutrient.nutrient)
             if not nutrient_req:
                 nutrient_req = 0
             else:
@@ -255,12 +296,13 @@ class FormulaNutrients(viewsets.ViewSet):
                 AND frq.formula_id = {formula_id};
         """.format(formula_id=formula.id))
 
-        columns = ['id', 'name', 'nutrient_group_id', 'ration_id','ration_value', 'requirement_id', 'requirement_value', 'achived_goal']
+        columns = ['id', 'name', 'nutrient_group_id', 'ration_id',
+                   'ration_value', 'requirement_id', 'requirement_value', 'achived_goal']
 
         return Response({
             'results': [dict(zip(columns, row)) for row in cursor.fetchall()]
         }, status=200)
-    
+
 
 class FormulaMatrix(viewsets.ViewSet):
     def get_queryset(self):
@@ -274,8 +316,10 @@ class FormulaMatrix(viewsets.ViewSet):
         queryset = models.FormulaIngredient.objects.filter(formula=formula)
         result = []
         for query in queryset.iterator():
-            ingredient_nutrients = IngredientNutrient.objects.filter(ingredient=query.ingredient.id).exclude(nutrient__nutrient_group__name="Energy")
-            nutrients = ingredient_nutrients.values_list('nutrient__name', flat=True)
+            ingredient_nutrients = IngredientNutrient.objects.filter(
+                ingredient=query.ingredient.id).exclude(nutrient__nutrient_group__name="Energy")
+            nutrients = ingredient_nutrients.values_list(
+                'nutrient__name', flat=True)
             values = ingredient_nutrients.values_list('value', flat=True)
             values = query.ration * np.array(values) / 100
 
@@ -294,13 +338,14 @@ class FormulaMatrix(viewsets.ViewSet):
 
         return Response({'results': result}, status=200)
 
+
 class FormulaIngredientAnalyses(viewsets.ViewSet):
     def get_queryset(self):
         try:
             return models.FormulaIngredient.all.get(formula=self.kwargs['formula_pk'], pk=self.kwargs['ingredient_pk'])
         except models.FormulaIngredient.DoesNotExist as ex:
             raise NotFound()
-        
+
     def list(self, request, formula_pk=None):
 
         return Response({}, status=200)