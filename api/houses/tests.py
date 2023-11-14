210f535adca55a819e4aab567075906a807741db
Simonbelete <simonbelete@gmail.com>
2023-11-14 16:31:29 +0300

    get house test

--- api/houses/tests.py
+++ api/houses/tests.py
@@ -13,6 +13,14 @@ class HouseAPITests(CoreAPITests):
         self.house = baker.prepare_recipe('houses.house')
         return super().setUp()
 
+    def test_get_houses(self):
+        url = reverse('api_house-list')
+        response = self.client.get(
+            url, format='json', headers=self.farm_admin_headers)
+
+        self.assertEqual(
+            json.loads(response.content)['count'], House.objects.all().count())
+
     def test_create_house(self):
         url = reverse('api_house-list')
         data = {
