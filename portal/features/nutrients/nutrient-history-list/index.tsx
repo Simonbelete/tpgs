d942ca9e00b0de35d7180f4867e27d18dedcd73f
Simonbelete <simonbelete@gmail.com>
2023-12-12 13:49:55 +0300

    nutrient history list

--- portal/features/nutrients/nutrient-history-list/index.tsx
+++ portal/features/nutrients/nutrient-history-list/index.tsx
@@ -5,34 +5,59 @@ import { nutrientApi } from "../services";
 import { Nutrient, NutrientHistory } from "@/models";
 import { IconButton, Tooltip, Typography, Button } from "@mui/material";
 import Link from "next/link";
+import dayjs from "dayjs";
 
 export const NutrientHistoryList = ({ data }: { data: Nutrient }) => {
   const columns: GridColDef[] = [
-    { field: "name", headerName: "Name" },
-    { field: "code", headerName: "Code" },
-    { field: "abbreviation", headerName: "Abbreviation" },
-    // {
-    //   field: "house",
-    //   headerName: "House",
-    //   flex: 1,
-    //   minWidth: 150,
-    //   valueGetter: (params) =>
-    //     params.row.nutrient_group ? params.row.nutrient_group.name : "",
-    //   renderCell: (params: GridRenderCellParams<any>) => {
-    //     if (params.row.house == null) return <></>;
-    //     return (
-    //       <Link href={`/houses/${params.row.house}`}>
-    //         <Tooltip title="Click to view">
-    //           <Button>
-    //             <Typography color={"link.primary"} variant="body2">
-    //               {params.row.house}
-    //             </Typography>
-    //           </Button>
-    //         </Tooltip>
-    //       </Link>
-    //     );
-    //   },
-    // },
+    { field: "name", headerName: "Name", flex: 1, minWidth: 150 },
+    {
+      field: "abbreviation",
+      headerName: "Abbreviation",
+      flex: 1,
+      minWidth: 150,
+    },
+    // { field: "description", headerName: "Description", flex: 1, minWidth: 150 },
+    {
+      field: "nutrient_group",
+      headerName: "Nutrient Group",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams<any>) => {
+        return (
+          <Typography color={"link.primary"} variant="body2">
+            <Link href={`/nutrient-groups/${params.row.nutrient_group}`}>
+              {params.row.nutrient_group}
+            </Link>
+          </Typography>
+        );
+      },
+    },
+    {
+      field: "unit",
+      headerName: "Unit",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams<any>) => {
+        return (
+          <Typography color={"link.primary"} variant="body2">
+            <Link href={`/units/${params.row.unit}`}>{params.row.unit}</Link>
+          </Typography>
+        );
+      },
+    },
+    { field: "order", headerName: "Order", flex: 1, minWidth: 100 },
+    {
+      field: "created_at",
+      headerName: "Create at",
+      flex: 1,
+      minWidth: 150,
+      valueGetter: (params) =>
+        params.row.created_at
+          ? dayjs(params.row.created_at).format(
+              process.env.NEXT_PUBLIC_DATE_FORMAT
+            )
+          : "",
+    },
   ];
   return (
     <HistoryList<NutrientHistory>
