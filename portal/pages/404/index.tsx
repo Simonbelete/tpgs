4bd303cc906e1273748deec1254d6a67fd540c8e
Simonbelete <simonbelete@gmail.com>
2024-01-25 21:20:34 +0300

    setting seo to error pages

--- portal/pages/404/index.tsx
+++ portal/pages/404/index.tsx
@@ -4,53 +4,57 @@ import { Button, Container, Box, Typography, Stack } from "@mui/material";
 import { useRouter } from "next/router";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import HomeIcon from "@mui/icons-material/Home";
+import { SeoHead } from "@/seo";
 
 const NotFoundPage = () => {
   const router = useRouter();
   return (
-    <Container maxWidth="sm">
-      <Box
-        display="flex"
-        flexDirection={"column"}
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-      >
-        <Typography
-          color="primary"
-          variant="h1"
-          component="span"
-          sx={{ fontWeight: 800, mb: 2 }}
+    <>
+      <SeoHead title="400" />
+      <Container maxWidth="sm">
+        <Box
+          display="flex"
+          flexDirection={"column"}
+          justifyContent="center"
+          alignItems="center"
+          minHeight="100vh"
         >
-          404
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="h6"
-          component="span"
-          sx={{ fontWeight: 800, mb: 5 }}
-        >
-          Page Not Found
-        </Typography>
-        <Stack gap={3} direction={"row"}>
-          <Link href={String(router.query.from || "/")}>
-            <Button
-              variant="outlined"
-              size="small"
-              startIcon={<KeyboardBackspaceIcon />}
-            >
-              Go Back
-            </Button>
-          </Link>
+          <Typography
+            color="primary"
+            variant="h1"
+            component="span"
+            sx={{ fontWeight: 800, mb: 2 }}
+          >
+            404
+          </Typography>
+          <Typography
+            color="textSecondary"
+            variant="h6"
+            component="span"
+            sx={{ fontWeight: 800, mb: 5 }}
+          >
+            Page Not Found
+          </Typography>
+          <Stack gap={3} direction={"row"}>
+            <Link href={String(router.query.from || "/")}>
+              <Button
+                variant="outlined"
+                size="small"
+                startIcon={<KeyboardBackspaceIcon />}
+              >
+                Go Back
+              </Button>
+            </Link>
 
-          <Link href={"/"}>
-            <Button variant="text" size="small" startIcon={<HomeIcon />}>
-              Home
-            </Button>
-          </Link>
-        </Stack>
-      </Box>
-    </Container>
+            <Link href={"/"}>
+              <Button variant="text" size="small" startIcon={<HomeIcon />}>
+                Home
+              </Button>
+            </Link>
+          </Stack>
+        </Box>
+      </Container>
+    </>
   );
 };
 
--- portal/pages/500/index.tsx
+++ portal/pages/500/index.tsx
@@ -4,53 +4,57 @@ import { useRouter } from "next/router";
 import { Button, Container, Box, Typography, Stack } from "@mui/material";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import HomeIcon from "@mui/icons-material/Home";
+import { SeoHead } from "@/seo";
 
 const ServerErrorFoundPage = () => {
   const router = useRouter();
   return (
-    <Container maxWidth="sm">
-      <Box
-        display="flex"
-        flexDirection={"column"}
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-      >
-        <Typography
-          color="primary"
-          variant="h1"
-          component="span"
-          sx={{ fontWeight: 800, mb: 2 }}
+    <>
+      <SeoHead title="500" />
+      <Container maxWidth="sm">
+        <Box
+          display="flex"
+          flexDirection={"column"}
+          justifyContent="center"
+          alignItems="center"
+          minHeight="100vh"
         >
-          500
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="h6"
-          component="span"
-          sx={{ fontWeight: 800, mb: 5 }}
-        >
-          Internal Server Error
-        </Typography>
-        <Stack gap={3} direction={"row"}>
-          <Link href={String(router.query.from || "/")}>
-            <Button
-              variant="outlined"
-              size="small"
-              startIcon={<KeyboardBackspaceIcon />}
-            >
-              Go Back
-            </Button>
-          </Link>
+          <Typography
+            color="primary"
+            variant="h1"
+            component="span"
+            sx={{ fontWeight: 800, mb: 2 }}
+          >
+            500
+          </Typography>
+          <Typography
+            color="textSecondary"
+            variant="h6"
+            component="span"
+            sx={{ fontWeight: 800, mb: 5 }}
+          >
+            Internal Server Error
+          </Typography>
+          <Stack gap={3} direction={"row"}>
+            <Link href={String(router.query.from || "/")}>
+              <Button
+                variant="outlined"
+                size="small"
+                startIcon={<KeyboardBackspaceIcon />}
+              >
+                Go Back
+              </Button>
+            </Link>
 
-          <Link href={"/"}>
-            <Button variant="text" size="small" startIcon={<HomeIcon />}>
-              Home
-            </Button>
-          </Link>
-        </Stack>
-      </Box>
-    </Container>
+            <Link href={"/"}>
+              <Button variant="text" size="small" startIcon={<HomeIcon />}>
+                Home
+              </Button>
+            </Link>
+          </Stack>
+        </Box>
+      </Container>
+    </>
   );
 };
 
