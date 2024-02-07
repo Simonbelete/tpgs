b773000bb60e299d93533a8b29d3770e8d4afe07
Simonbelete <simonbelete@gmail.com>
2024-02-07 21:22:28 +0300

    modify cookie consent

--- portal/components/CookieConsent.tsx
+++ portal/components/CookieConsent.tsx
@@ -5,13 +5,19 @@ const CookieConsentComponent = () => {
   return (
     <CookieConsent
       location="bottom"
-      buttonText="I understand!"
+      buttonText="I understand"
       cookieName="cookieConsent"
       style={{ background: "#2B373B" }}
       buttonStyle={{ background: "#008a3f", color: "#fff", fontSize: "13px" }}
       expires={150}
     >
-      This website uses cookies to enhance the user experience.{" "}
+      <span style={{ fontFamily: "Inter,sans-serif", fontWeight: 600 }}>
+        This website uses cookies.{" "}
+      </span>
+      <span style={{ fontSize: "13px", fontFamily: "Inter,sans-serif" }}>
+        We use cookies to enhance your browsing and analyze our traffic. By
+        clicking &quot;I understand&quot;, you consent to our use of cookies.
+      </span>
     </CookieConsent>
   );
 };
