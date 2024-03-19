2f0316bf42356e8e007d06e0f628ac1b84a00885
Simonbelete <simonbelete@gmail.com>
2024-03-19 10:56:18 +0300

    fix migrations

--- api/chickens/migrations/0002_initial.py
+++ api/chickens/migrations/0002_initial.py
@@ -22,71 +22,99 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='historicalchicken',
             name='created_by',
-            field=models.ForeignKey(blank=True, db_constraint=False, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(blank=True, db_constraint=False, null=True,
+                                    on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
             model_name='historicalchicken',
             name='dam',
-            field=models.ForeignKey(blank=True, db_constraint=False, limit_choices_to={'sex': 'F'}, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='chickens.chicken'),
+            field=models.ForeignKey(blank=True, db_constraint=False, limit_choices_to={
+                                    'sex': 'F'}, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='chickens.chicken'),
         ),
         migrations.AddField(
             model_name='historicalchicken',
             name='hatchery',
-            field=models.ForeignKey(blank=True, db_constraint=False, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='hatchery.hatchery'),
+            field=models.ForeignKey(blank=True, db_constraint=False, null=True,
+                                    on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='hatchery.hatchery'),
         ),
         migrations.AddField(
             model_name='historicalchicken',
             name='history_user',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL,
+                                    related_name='+', to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
             model_name='historicalchicken',
             name='pen',
-            field=models.ForeignKey(blank=True, db_constraint=False, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='pen.pen'),
+            field=models.ForeignKey(blank=True, db_constraint=False, null=True,
+                                    on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='pen.pen'),
         ),
         migrations.AddField(
             model_name='historicalchicken',
             name='reduction_reason',
-            field=models.ForeignKey(blank=True, db_constraint=False, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='reduction_reason.reductionreason'),
+            field=models.ForeignKey(blank=True, db_constraint=False, null=True,
+                                    on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='reduction_reason.reductionreason'),
         ),
         migrations.AddField(
             model_name='historicalchicken',
             name='sire',
-            field=models.ForeignKey(blank=True, db_constraint=False, limit_choices_to={'sex': 'M'}, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='chickens.chicken'),
+            field=models.ForeignKey(blank=True, db_constraint=False, limit_choices_to={
+                                    'sex': 'M'}, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='chickens.chicken'),
         ),
         migrations.AddField(
             model_name='chicken',
             name='breed',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='breeds.breed'),
+            field=models.ForeignKey(
+                blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='breeds.breed'),
         ),
         migrations.AddField(
             model_name='chicken',
             name='created_by',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(
+                null=True, on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
             model_name='chicken',
             name='dam',
-            field=models.ForeignKey(blank=True, limit_choices_to={'sex': 'F'}, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='children_of_dam', to='chickens.chicken'),
+            field=models.ForeignKey(blank=True, limit_choices_to={
+                                    'sex': 'F'}, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='children_of_dam', to='chickens.chicken'),
         ),
         migrations.AddField(
             model_name='chicken',
             name='hatchery',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='chickens', to='hatchery.hatchery'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL,
+                                    related_name='chickens', to='hatchery.hatchery'),
         ),
         migrations.AddField(
             model_name='chicken',
             name='pen',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='chickens', to='pen.pen'),
+            field=models.ForeignKey(
+                blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='chickens', to='pen.pen'),
         ),
         migrations.AddField(
             model_name='chicken',
             name='reduction_reason',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='chickens', to='reduction_reason.reductionreason'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL,
+                                    related_name='chickens', to='reduction_reason.reductionreason'),
         ),
         migrations.AddField(
             model_name='chicken',
             name='sire',
-            field=models.ForeignKey(blank=True, limit_choices_to={'sex': 'M'}, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='children_of_sire', to='chickens.chicken'),
+            field=models.ForeignKey(blank=True, limit_choices_to={
+                                    'sex': 'M'}, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='children_of_sire', to='chickens.chicken'),
+        ),
+        migrations.AddField(
+            model_name='chicken',
+            name='color',
+            field=models.TextField(blank=True, default=''),
+        ),
+        migrations.AddField(
+            model_name='historicalchicken',
+            name='color',
+            field=models.TextField(blank=True, default=''),
+        ),
+        migrations.AlterModelOptions(
+            name='chicken',
+            options={'ordering': ['-created_at']},
         ),
     ]
