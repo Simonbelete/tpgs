be9b8f951b75d9c2f64fdc84639a49b2f73535da
Simon Belete <simonbelete@gmail.com>
2024-04-30 23:34:01 +0300

    pwa and seo

--- portal/data/siteMetadata.js
+++ portal/data/siteMetadata.js
@@ -3,8 +3,8 @@ const siteMetadata = {
   headerTitle: "ILRI TPGS",
   siteUrl: "https://tpgs.ilri.org/",
   description:
-    "Tropical Poultry Genetics Solutions (TPGS): Delivering farmer preferred, productive and ecologically adapted poultry to smallholders. The project will build on the gains of ACGG and its bridging phase that concluded in April 2022.",
-  image: "https://tpgs.ilri.org/images/chickens_image.jpg",
+    "Tropical Poultry Genetics Solutions (TPGS): Delivering farmer preferred, productive and ecologically adapted poultry to smallholders. The project will build on the gains of ACGG and its bridging phase that concluded in April 2022. In addition, the new phase will benefit from on-going activities of the Asian Chicken Genetic Gains (AsCGG) project in South-East Asia. This new phase shifts the focus towards impact, intervention through the establishment of new partnerships, and the close involvement of investors.",
+  image: "https://tpgs.ilri.org/images/hero_3.png",
   local: "en_US",
 };
 
--- portal/seo.tsx
+++ portal/seo.tsx
@@ -50,6 +50,30 @@ export const SeoHead = ({
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+      <meta name="theme-color" content="#008a3f" />
+
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/tpsg_platforms_icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/tpsg_platforms_icon.png"
+      />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+
+      <meta name="twitter:card" content={siteMetadata.description} />
+      <meta name="twitter:url" content={siteMetadata.siteUrl} />
+      <meta name="twitter:title" content={`${title} | ${siteMetadata.title}`} />
+      <meta name="twitter:description" content={siteMetadata.description} />
+      <meta name="twitter:image" content={image ? image : siteMetadata.image} />
+      <meta name="twitter:creator" content="@DavidWShadow" />
+
       <meta property="og:locale" content={siteMetadata.local} />
       <meta property="og:site_name" content={siteMetadata.title} />
       <meta property="og:type" content={"website"} />
